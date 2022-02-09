dd038c5e5382fb8a52ec303a1a51a34d83fbffba
zhouzaida <zhouzaida@163.com>
2022-02-09 17:09:08 +0800

    fix the format

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -12,10 +12,10 @@
 #
 import os
 import sys
+
 import pytorch_sphinx_theme
 
 sys.path.insert(0, os.path.abspath('../..'))
-
 
 # -- Project information -----------------------------------------------------
 
@@ -56,7 +56,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -73,8 +72,7 @@ html_theme_options = {
         },
     ],
     # Specify the language of shared menu
-    'menu_lang':
-    'en',
+    'menu_lang': 'en',
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
