0956438fdf7a1ec5d403ed5a8bda26daba03d4af
zhouzaida <zhouzaida@163.com>
2022-02-12 22:37:11 +0800

    add root registry

--- mmengine/registry/__init__.py
+++ mmengine/registry/__init__.py
@@ -1,4 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .registry import Registry, build_from_cfg
+from .root import (DATASETS, HOOKS, MODELS, OPTIMIZER_CONSTRUCTORS, OPTIMIZERS,
+                   PIPELINES, RUNNER_CONSTRUCTORS, RUNNERS, SAMPLERS,
+                   TASK_UTILS, WEIGHT_INITIALIZERS)
 
-__all__ = ['Registry', 'build_from_cfg']
+__all__ = [
+    'Registry', 'build_from_cfg', 'RUNNERS', 'RUNNER_CONSTRUCTORS', 'HOOKS',
+    'DATASETS', 'SAMPLERS', 'PIPELINES', 'MODELS', 'WEIGHT_INITIALIZERS',
+    'OPTIMIZERS', 'OPTIMIZER_CONSTRUCTORS', 'TASK_UTILS'
+]
---
+++ mmengine/registry/root.py
@@ -0,0 +1,18 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .registry import Registry
+
+RUNNERS = Registry('runner')
+RUNNER_CONSTRUCTORS = Registry('runner constructor')
+HOOKS = Registry('hook')
+
+DATASETS = Registry('dataset')
+SAMPLERS = Registry('sampler')
+PIPELINES = Registry('pipeline')
+
+MODELS = Registry('model')
+WEIGHT_INITIALIZERS = Registry('weight initializer')
+
+OPTIMIZERS = Registry('optimizer')
+OPTIMIZER_CONSTRUCTORS = Registry('optimizer constructor')
+
+TASK_UTILS = Registry('task util')
