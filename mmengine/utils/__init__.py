11976821630fea9ecec748a62e042120d0307169
HAOCHENYE <21724054@zju.edu.cn>
2022-02-14 18:33:32 +0800

    tmp add fileio

---
+++ mmengine/utils/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+# type: ignore
+from .fileio import (FileClient, dict_from_file, dump, list_from_file, load,
+                     register_handler)
+from .misc import (check_prerequisites, concat_list, deprecated_api_warning,
+                   has_method, import_modules_from_strings, is_list_of,
+                   is_method_overridden, is_seq_of, is_str, is_tuple_of,
+                   iter_cast, list_cast, requires_executable, requires_package,
+                   slice_list, to_1tuple, to_2tuple, to_3tuple, to_4tuple,
+                   to_ntuple, tuple_cast)
+from .path import (check_file_exist, fopen, is_filepath, mkdir_or_exist,
+                   scandir, symlink)
+
+__all__ = [
+    'is_str', 'iter_cast', 'list_cast', 'tuple_cast', 'is_seq_of',
+    'is_list_of', 'is_tuple_of', 'slice_list', 'concat_list',
+    'check_prerequisites', 'requires_package', 'requires_executable',
+    'is_filepath', 'fopen', 'check_file_exist', 'mkdir_or_exist', 'symlink',
+    'scandir', 'deprecated_api_warning', 'import_modules_from_strings',
+    'to_1tuple', 'to_2tuple', 'to_3tuple', 'to_4tuple', 'to_ntuple',
+    'is_method_overridden', 'has_method', 'dict_from_file', 'list_from_file',
+    'register_handler', 'dump', 'load', 'FileClient'
+]
