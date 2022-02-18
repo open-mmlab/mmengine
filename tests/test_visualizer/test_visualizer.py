e7588322270b0dbd89b4606a6b76cf484da67879
hha <1286304229@qq.com>
2022-02-18 22:33:53 +0800

    fix commit

--- tests/test_visualizer/test_visualizer.py
+++ tests/test_visualizer/test_visualizer.py
@@ -50,50 +50,103 @@ class TesVisualizer(TestCase):
     def test_draw_bboxes(self):
         visualizer = Visualizer(image=self.image)
 
-        #  4 or nx4
-        bboxes = np.array([[1, 1, 2, 2], [1, 1.5, 1, 2.5]])
+        #  only support 4 or nx4 tensor and numpy
+        bboxes = torch.tensor([1, 1, 2, 2])
         visualizer.draw_bboxes(bboxes)
+        bboxes = torch.tensor([[1, 1, 2, 2], [1, 1.5, 1, 2.5]])
+        visualizer.draw_bboxes(
+            bboxes, alpha=0.5, edge_color='b', line_style='-')
 
-        # [x1,y1,x2,y2] or [[x11,y11,x12,y12],[x21,y21,x22,y22]]
-        bboxes = [1, 1, 2, 2]
-        visualizer.draw_bboxes(bboxes)
-        bboxes = [[1, 1, 2, 2], [1, 1.5, 1, 2.5]]
+        bboxes = bboxes.numpy()
         visualizer.draw_bboxes(bboxes)
 
         # test incorrect bbox format
+        # list is not supported
         with pytest.raises(AssertionError):
-            bboxes = [1, 1, 2, 2, 1]
+            bboxes = [1, 1, 2, 2]
             visualizer.draw_bboxes(bboxes)
 
     def test_draw_texts(self):
         visualizer = Visualizer(image=self.image)
-        visualizer.draw_texts('test', [5, 5])
+        visualizer.draw_texts('text1', position=[5, 5])
+        visualizer.draw_texts(['text1', 'text2'], position=[[5, 5], [3, 3]])
+        visualizer.draw_texts(['text1', 'text2'], position=([5, 5], [3, 3]))
+        visualizer.draw_texts(('text1', 'text2'), position=([5, 5], [3, 3]))
+        visualizer.draw_texts('text1', position=torch.tensor([5, 5]))
+        visualizer.draw_texts(['text1', 'text2'],
+                              position=torch.tensor([[5, 5], [3, 3]]))
+        visualizer.draw_texts('text1', position=np.array([5, 5]))
+        visualizer.draw_texts(['text1', 'text2'],
+                              position=np.array([[5, 5], [3, 3]]))
+
+        # test length mismatch
+        with pytest.raises(AssertionError):
+            visualizer.draw_texts(['text1', 'text2'], position=[5, 5])
+            visualizer.draw_texts('text1', position=[[5, 5]])
+            visualizer.draw_texts('text1', position=[[5, 5], [3, 3]])
 
     def test_draw_lines(self):
         visualizer = Visualizer(image=self.image)
-        visualizer.draw_lines([1, 5])
+
+        visualizer.draw_lines(x_datas=[1, 5], y_datas=[2, 6])
+        visualizer.draw_lines(x_datas=[1, 5, 4], y_datas=[2, 6, 6])
+        visualizer.draw_lines(x_datas=(1, 5), y_datas=(2, 6))
+        visualizer.draw_lines(
+            x_datas=torch.tensor([1, 5]), y_datas=torch.tensor([2, 6]))
+        visualizer.draw_lines(
+            x_datas=np.array([1, 5, 4]), y_datas=np.array([2, 6, 6]))
+
+        # test length mismatch
+        with pytest.raises(AssertionError):
+            visualizer.draw_lines(x_datas=[1, 5], y_datas=[2, 6, 7])
 
     def test_draw_circles(self):
         visualizer = Visualizer(image=self.image)
-        visualizer.draw_circles([1, 5])
+        visualizer.draw_circles([1, 5], radius=1)
+        visualizer.draw_circles((1, 5), radius=1)
+        visualizer.draw_circles([[1, 5], [2, 6]], radius=[1, 2])
+        visualizer.draw_circles(torch.tensor([1, 5]))
+        visualizer.draw_circles(np.array([1, 5]))
+
+        # test length mismatch
+        with pytest.raises(AssertionError):
+            visualizer.draw_circles([1, 5], radius=[1, 2])
 
     def test_draw_polygons(self):
         visualizer = Visualizer(image=self.image)
-        visualizer.draw_polygons([1, 5])
+        # shape Nx2 or list[Nx2]
+        visualizer.draw_polygons(torch.tensor([[1, 1], [2, 2], [3, 4]]))
+        visualizer.draw_polygons(np.array([[1, 1], [2, 2], [3, 4]]))
+        visualizer.draw_polygons([
+            np.array([[1, 1], [2, 2], [3, 4]]),
+            torch.tensor([[1, 1], [2, 2], [3, 4]])
+        ])
 
     def test_draw_binary_masks(self):
-        binary_mask = np.random.randint(0, 2, size=(10, 10, 3)).astype(np.bool)
+        binary_mask = np.random.randint(0, 2, size=(10, 10)).astype(np.bool)
         visualizer = Visualizer(image=self.image)
         visualizer.draw_binary_masks(binary_mask)
+        visualizer.draw_binary_masks(torch.from_numpy(binary_mask))
 
         # test the error that the size of mask and image are different.
         with pytest.raises(AssertionError):
-            binary_mask = np.random.randint(
-                0, 2, size=(8, 10, 3)).astype(np.bool)
+            binary_mask = np.random.randint(0, 2, size=(8, 10)).astype(np.bool)
             visualizer.draw_binary_masks(binary_mask)
 
-    # TODO
+        # test non binary mask error
+        binary_mask = np.random.randint(0, 2, size=(10, 10, 3)).astype(np.bool)
+        with pytest.raises(AssertionError):
+            visualizer.draw_binary_masks(binary_mask)
+
+        # test non bool error
+        binary_mask = np.random.randint(0, 2, size=(10, 10))
+        with pytest.raises(AssertionError):
+            visualizer.draw_binary_masks(binary_mask)
+
     def test_draw_featmap(self):
+        # Supports tensor in BCHW format where channel C is 1 or 3
+        # Supports tensor in BCHW format with channel C greater than 3
+        # Support tensor in CHW format
         visualizer = Visualizer()
         with pytest.raises(AssertionError):
             featmap = torch.randn(2, 2, 3, 3)
@@ -103,15 +156,17 @@ class TesVisualizer(TestCase):
         visualizer.draw_featmap(featmap)
         featmap = torch.randn(2, 1, 3, 3)
         visualizer.draw_featmap(featmap)
+        featmap = torch.randn(5, 3, 3)
+        visualizer.draw_featmap(featmap, topk=3)
 
     def test_chain_call(self):
         visualizer = Visualizer(image=self.image)
-        binary_mask = np.random.randint(0, 2, size=(10, 10, 3)).astype(np.bool)
-        visualizer.draw_bboxes([1, 1, 2, 2]). \
+        binary_mask = np.random.randint(0, 2, size=(10, 10)).astype(np.bool)
+        visualizer.draw_bboxes(torch.tensor([1, 1, 2, 2])). \
             draw_texts('test', [5, 5]). \
-            draw_lines([1, 5]). \
+            draw_lines([1, 5], [2, 6]). \
             draw_circles([1, 5]). \
-            draw_polygons([1, 5]). \
+            draw_polygons(torch.tensor([[1, 1], [2, 2], [3, 4]])). \
             draw_binary_masks(binary_mask)
 
     def test_register_task(self):
