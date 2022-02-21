7ecc7f2f27020a9622ca67035d867237e411a370
PJLAB\huanghaian <1286304229@qq.com>
2022-02-21 11:05:57 +0800

    fix comment

--- tests/test_visualizer/test_visualizer.py
+++ tests/test_visualizer/test_visualizer.py
@@ -34,13 +34,15 @@ class TesVisualizer(TestCase):
         visualizer = Visualizer(scale=2, image=self.image)
         out_image = visualizer.get_image()
         assert (20, 20, 3) == out_image.shape
-        assert visualizer.experiment == visualizer
 
     def test_set_image(self):
         visualizer = Visualizer()
         visualizer.set_image(self.image)
+        assert self.assert_img_equal(self.image, visualizer.get_image())
         # test grayscale image
         visualizer.set_image(self.image[..., 0])
+        assert self.assert_img_equal(self.image[..., 0],
+                                     visualizer.get_image())
 
     def test_get_image(self):
         visualizer = Visualizer(image=self.image)
@@ -50,28 +52,24 @@ class TesVisualizer(TestCase):
     def test_draw_bboxes(self):
         visualizer = Visualizer(image=self.image)
 
-        #  only support 4 or nx4 tensor and numpy
+        # only support 4 or nx4 tensor and numpy
         bboxes = torch.tensor([1, 1, 2, 2])
         visualizer.draw_bboxes(bboxes)
         bboxes = torch.tensor([[1, 1, 2, 2], [1, 1.5, 1, 2.5]])
         visualizer.draw_bboxes(
             bboxes, alpha=0.5, edge_color='b', line_style='-')
-
         bboxes = bboxes.numpy()
         visualizer.draw_bboxes(bboxes)
 
         # test incorrect bbox format
-        # list is not supported
         with pytest.raises(AssertionError):
             bboxes = [1, 1, 2, 2]
             visualizer.draw_bboxes(bboxes)
 
     def test_draw_texts(self):
         visualizer = Visualizer(image=self.image)
-        visualizer.draw_texts('text1', position=[5, 5])
-        visualizer.draw_texts(['text1', 'text2'], position=[[5, 5], [3, 3]])
-        visualizer.draw_texts(['text1', 'text2'], position=([5, 5], [3, 3]))
-        visualizer.draw_texts(('text1', 'text2'), position=([5, 5], [3, 3]))
+
+        # only support tensor and numpy
         visualizer.draw_texts('text1', position=torch.tensor([5, 5]))
         visualizer.draw_texts(['text1', 'text2'],
                               position=torch.tensor([[5, 5], [3, 3]]))
@@ -79,38 +77,53 @@ class TesVisualizer(TestCase):
         visualizer.draw_texts(['text1', 'text2'],
                               position=np.array([[5, 5], [3, 3]]))
 
+        # test incorrect format
+        with pytest.raises(AssertionError):
+            visualizer.draw_texts('text', position=[5, 5])
+
         # test length mismatch
         with pytest.raises(AssertionError):
-            visualizer.draw_texts(['text1', 'text2'], position=[5, 5])
-            visualizer.draw_texts('text1', position=[[5, 5]])
-            visualizer.draw_texts('text1', position=[[5, 5], [3, 3]])
+            visualizer.draw_texts(['text1', 'text2'],
+                                  position=torch.tensor([5, 5]))
+            visualizer.draw_texts('text1', position=torch.tensor([[5, 5]]))
+            visualizer.draw_texts(
+                'text1', position=torch.tensor([[5, 5], [3, 3]]))
 
     def test_draw_lines(self):
         visualizer = Visualizer(image=self.image)
 
-        visualizer.draw_lines(x_datas=[1, 5], y_datas=[2, 6])
-        visualizer.draw_lines(x_datas=[1, 5, 4], y_datas=[2, 6, 6])
-        visualizer.draw_lines(x_datas=(1, 5), y_datas=(2, 6))
+        # only support tensor and numpy
         visualizer.draw_lines(
             x_datas=torch.tensor([1, 5]), y_datas=torch.tensor([2, 6]))
         visualizer.draw_lines(
             x_datas=np.array([1, 5, 4]), y_datas=np.array([2, 6, 6]))
 
+        # test incorrect format
+        with pytest.raises(AssertionError):
+            visualizer.draw_texts('text', position=[5, 5])
+
         # test length mismatch
         with pytest.raises(AssertionError):
-            visualizer.draw_lines(x_datas=[1, 5], y_datas=[2, 6, 7])
+            visualizer.draw_lines(
+                x_datas=torch.tensor([1, 5]), y_datas=torch.tensor([2, 6, 7]))
 
     def test_draw_circles(self):
         visualizer = Visualizer(image=self.image)
-        visualizer.draw_circles([1, 5], radius=1)
-        visualizer.draw_circles((1, 5), radius=1)
-        visualizer.draw_circles([[1, 5], [2, 6]], radius=[1, 2])
+
+        # only support tensor and numpy
         visualizer.draw_circles(torch.tensor([1, 5]))
         visualizer.draw_circles(np.array([1, 5]))
+        visualizer.draw_circles(
+            torch.tensor([[1, 5], [2, 6]]), radius=torch.tensor([1, 2]))
+
+        # test incorrect format
+        with pytest.raises(AssertionError):
+            visualizer.draw_circles([1, 5])
 
         # test length mismatch
         with pytest.raises(AssertionError):
-            visualizer.draw_circles([1, 5], radius=[1, 2])
+            visualizer.draw_circles(
+                torch.tensor([[1, 5]]), radius=torch.tensor([1, 2]))
 
     def test_draw_polygons(self):
         visualizer = Visualizer(image=self.image)
@@ -144,28 +157,44 @@ class TesVisualizer(TestCase):
             visualizer.draw_binary_masks(binary_mask)
 
     def test_draw_featmap(self):
-        # Supports tensor in BCHW format where channel C is 1 or 3
-        # Supports tensor in BCHW format with channel C greater than 3
-        # Support tensor in CHW format
         visualizer = Visualizer()
-        with pytest.raises(AssertionError):
-            featmap = torch.randn(2, 2, 3, 3)
-            visualizer.draw_featmap(featmap)
 
-        featmap = torch.randn(2, 6, 3, 3)
-        visualizer.draw_featmap(featmap)
-        featmap = torch.randn(2, 1, 3, 3)
-        visualizer.draw_featmap(featmap)
-        featmap = torch.randn(5, 3, 3)
-        visualizer.draw_featmap(featmap, topk=3)
+        # test tensor format
+        with pytest.raises(AssertionError, match='Input dimension must be 3'):
+            visualizer.draw_featmap(torch.randn(1, 1, 3, 3))
+
+        # test mode parameter
+        # mode only supports 'mean' and 'max'
+        with pytest.raises(AssertionError):
+            visualizer.draw_featmap(torch.randn(2, 3, 3), mode='min')
+
+        # test topk parameter
+        with pytest.raises(
+                AssertionError,
+                match='The input tensor channel dimension must be 1 or 3 '
+                'when topk is less than 1, but the channel '
+                'dimension you input is 6, you can use the '
+                'mode parameter or set topk greater than 0 to solve '
+                'the error'):
+            visualizer.draw_featmap(torch.randn(6, 3, 3), mode=None, topk=0)
+
+        visualizer.draw_featmap(torch.randn(6, 3, 3), mode='mean')
+        visualizer.draw_featmap(torch.randn(1, 3, 3), mode='mean')
+        visualizer.draw_featmap(torch.randn(6, 3, 3), mode='max')
+        visualizer.draw_featmap(torch.randn(6, 3, 3), mode='max', topk=10)
+        visualizer.draw_featmap(torch.randn(1, 3, 3), mode=None, topk=-1)
+        visualizer.draw_featmap(torch.randn(3, 3, 3), mode=None, topk=-1)
+        visualizer.draw_featmap(torch.randn(6, 3, 3), mode=None, topk=4)
+        visualizer.draw_featmap(torch.randn(6, 3, 3), mode=None, topk=8)
 
     def test_chain_call(self):
         visualizer = Visualizer(image=self.image)
         binary_mask = np.random.randint(0, 2, size=(10, 10)).astype(np.bool)
         visualizer.draw_bboxes(torch.tensor([1, 1, 2, 2])). \
-            draw_texts('test', [5, 5]). \
-            draw_lines([1, 5], [2, 6]). \
-            draw_circles([1, 5]). \
+            draw_texts('test', torch.tensor([5, 5])). \
+            draw_lines(x_datas=torch.tensor([1, 5]),
+                       y_datas=torch.tensor([2, 6])). \
+            draw_circles(torch.tensor([1, 5])). \
             draw_polygons(torch.tensor([[1, 1], [2, 2], [3, 4]])). \
             draw_binary_masks(binary_mask)
 
@@ -183,7 +212,7 @@ class TesVisualizer(TestCase):
         # test registration of the same names.
         with pytest.raises(
                 KeyError,
-                match=('instances is already registered in task_dict, '
+                match=('"instances" is already registered in task_dict, '
                        'add "force=True" if you want to override it')):
 
             class DetVisualizer1(Visualizer):
