709986b0e42529a5a214cacfaae60db2b272f804
liukuikun <liukuikun@sensetime.com>
2022-02-22 12:59:55 +0800

    split  into base_data_element and base_data_sample

--- mmengine/data/__init__.py
+++ mmengine/data/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .data_structures import BaseDataElement, BaseDataSample
+from .base_data_element import BaseDataElement
+from .base_data_sample import BaseDataSample
 from .sampler import DefaultSampler, InfiniteSampler
 
 __all__ = [
