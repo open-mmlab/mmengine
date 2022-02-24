0e31db1739fa7c881b17cc6db5ac7671ad61af48
plyfager <2744335995@qq.com>
2022-02-24 21:03:13 +0800

    fix mypy

--- mmengine/hooks/iter_timer_hook.py
+++ mmengine/hooks/iter_timer_hook.py
@@ -11,14 +11,24 @@ class IterTimerHook(Hook):
     Eg. ``data_time`` for loading data and ``time`` for a model train step.
     """
 
-    def before_epoch(self, runner):
+    def before_epoch(self, runner: object) -> None:
+        """Record time flag before start a epoch."""
         self.t = time.time()
 
-    def before_iter(self, runner):
+    def before_iter(self, runner: object, data_batch=None) -> None:
+        """Logging time for loading data and update the time flag."""
         # TODO: update for new logging system
-        runner.log_buffer.update({'data_time': time.time() - self.t})
+        runner.log_buffer.update({  # type: ignore
+            'data_time': time.time() - self.t
+        })
 
-    def after_iter(self, runner):
+    def after_iter(self,
+                   runner: object,
+                   data_batch=None,
+                   outputs=None) -> None:
+        """Logging time for a iteration and update the time flag."""
         # TODO: update for new logging system
-        runner.log_buffer.update({'time': time.time() - self.t})
+        runner.log_buffer.update({  # type: ignore
+            'time': time.time() - self.t
+        })
         self.t = time.time()
