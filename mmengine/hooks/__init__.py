68e76090350d7dc6d2e55699934cd215fe6f644c
seuyou <3463423099@qq.com>
2022-02-24 12:56:59 +0800

    [Feature]: Add Hook

--- mmengine/hooks/__init__.py
+++ mmengine/hooks/__init__.py
@@ -1,11 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .checkpoint_hook import CheckpointHook
 from .hook import HOOKS, Hook
-from .optimizer_hook import OptimizerHook
-from .param_scheduler_hook import ParamSchedulerHook
-from .sampler_seed_hook import DistSamplerSeedHook
 
-__all__ = [
-    'HOOKS', 'Hook', 'OptimizerHook', 'CheckpointHook', 'ParamSchedulerHook',
-    'DistSamplerSeedHook'
-]
+__all__ = ['HOOKS', 'Hook']
