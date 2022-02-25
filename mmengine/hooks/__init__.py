c69e782c97be19232e767002fed2aab02c3215f8
plyfager <2744335995@qq.com>
2022-02-25 20:27:34 +0800

    fix lint

--- mmengine/hooks/__init__.py
+++ mmengine/hooks/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .empty_cache_hook import EmptyCacheHook
 from .hook import HOOKS, Hook
 
-__all__ = ['HOOKS', 'Hook']
+__all__ = ['HOOKS', 'Hook', 'EmptyCacheHook']
---
+++ tests/test_hook/test_empty_cache_hook.py
@@ -0,0 +1,17 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import pytest
+import torch
+from mock import Mock
+
+from mmengine.hooks import EmptyCacheHook
+
+
+class TestEmptyCacheHook:
+
+    @pytest.mark.skipif(condition=not torch.cuda.is_available(), reason='')
+    def test_emtpy_cache_hook(self):
+        Hook = EmptyCacheHook(True, True, True)
+        Runner = Mock()
+        Hook.after_iter(Runner)
+        Hook.before_epoch(Runner)
+        Hook.after_epoch(Runner)
