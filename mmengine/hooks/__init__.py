46e9b4140ba5c0fe433ed78ab05ba94de7c4343b
seuyou <3463423099@qq.com>
2022-02-28 10:52:23 +0800

    [Feature]: Add optimizer hook

--- mmengine/hooks/__init__.py
+++ mmengine/hooks/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .hook import Hook
 from .iter_timer_hook import IterTimerHook
+from .optimizer_hook import OptimizerHook
 
-__all__ = ['Hook', 'IterTimerHook']
+__all__ = ['Hook', 'IterTimerHook', 'OptimizerHook']
