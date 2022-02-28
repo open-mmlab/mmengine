a5b0c6f14e4895cc80c7f5b0b8761891ecfe9d41
plyfager <2744335995@qq.com>
2022-02-28 13:53:57 +0800

    improve doc coverage and merge main

--- mmengine/hooks/iter_timer_hook.py
+++ mmengine/hooks/iter_timer_hook.py
@@ -1,7 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import time
+from typing import Optional, Sequence
 
-from .hook import HOOKS, Hook
+from mmengine.data import BaseDataSample
+from mmengine.registry import HOOKS
+from .hook import Hook
 
 
 @HOOKS.register_module()
@@ -12,11 +15,24 @@ class IterTimerHook(Hook):
     """
 
     def before_epoch(self, runner: object) -> None:
-        """Record time flag before start a epoch."""
+        """Record time flag before start a epoch.
+
+        Args:
+            runner (object): The runner of the training process.
+        """
         self.t = time.time()
 
-    def before_iter(self, runner: object, data_batch=None) -> None:
-        """Logging time for loading data and update the time flag."""
+    def before_iter(
+            self,
+            runner: object,
+            data_batch: Optional[Sequence[BaseDataSample]] = None) -> None:
+        """Logging time for loading data and update the time flag.
+
+        Args:
+            runner (object): The runner of the training process.
+            data_batch (Sequence[BaseDataSample]): Data from dataloader.
+                Defaults to None.
+        """
         # TODO: update for new logging system
         runner.log_buffer.update({  # type: ignore
             'data_time': time.time() - self.t
@@ -24,9 +40,17 @@ class IterTimerHook(Hook):
 
     def after_iter(self,
                    runner: object,
-                   data_batch=None,
-                   outputs=None) -> None:
-        """Logging time for a iteration and update the time flag."""
+                   data_batch: Optional[Sequence[BaseDataSample]] = None,
+                   outputs: Optional[Sequence[BaseDataSample]] = None) -> None:
+        """Logging time for a iteration and update the time flag.
+
+        Args:
+            runner (object): The runner of the training process.
+            data_batch (Sequence[BaseDataSample]): Data from dataloader.
+                Defaults to None.
+            outputs (Sequence[BaseDataSample]): Outputs from model.
+                Defaults to None.
+        """
         # TODO: update for new logging system
         runner.log_buffer.update({  # type: ignore
             'time': time.time() - self.t
