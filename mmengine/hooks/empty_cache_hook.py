1b0315ef0f759663bfc2619f2746b9084e271dee
plyfager <2744335995@qq.com>
2022-02-28 14:59:18 +0800

    update typing and docs

--- mmengine/hooks/empty_cache_hook.py
+++ mmengine/hooks/empty_cache_hook.py
@@ -1,25 +1,59 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import torch
 
-from .hook import HOOKS, Hook
+from .hook import Hook
+from mmengine.registry import HOOKS
 
 
 @HOOKS.register_module()
 class EmptyCacheHook(Hook):
+    """Releases all unoccupied cached GPU memory during the process of 
+        training.
 
-    def __init__(self, before_epoch=False, after_epoch=True, after_iter=False):
+    Args:
+        before_epoch (bool): Whether empty cache before an epoch. Defaults to 
+            False.
+        after_epoch (bool): Whether empty cache after an epoch. Defaults to 
+            True.
+        after_iter (bool): Whether empty cache after an iteration. Defaults to
+            False.
+    """
+
+    def __init__(self,
+                 before_epoch: bool = False,
+                 after_epoch: bool = True,
+                 after_iter: bool = False) -> None:
         self._before_epoch = before_epoch
         self._after_epoch = after_epoch
         self._after_iter = after_iter
 
-    def after_iter(self, runner):
+    def after_iter(self, runner: object) -> None:
+        """Empty cache after an iteration.
+
+        Args:
+            runner (object): The runner of the training process.
+            data_batch (Sequence[BaseDataSample]): Data from dataloader.
+                Defaults to None.
+            outputs (Sequence[BaseDataSample]): Outputs from model.
+                Defaults to None.
+        """
         if self._after_iter:
             torch.cuda.empty_cache()
 
-    def before_epoch(self, runner):
+    def before_epoch(self, runner: object) -> None:
+        """Empty cache before an epoch.
+
+        Args:
+            runner (object): The runner of the training process.
+        """
         if self._before_epoch:
             torch.cuda.empty_cache()
 
-    def after_epoch(self, runner):
+    def after_epoch(self, runner: object) -> None:
+        """Empty cache after an epoch.
+
+        Args:
+            runner (object): The runner of the training process.
+        """
         if self._after_epoch:
             torch.cuda.empty_cache()
