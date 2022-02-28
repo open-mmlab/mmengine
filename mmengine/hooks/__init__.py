aad4a87440a046c93814c8973ae0052e14e31be0
seuyou <3463423099@qq.com>
2022-02-28 10:52:23 +0800

    [Feature]: Add optimizer hook

--- mmengine/hooks/__init__.py
+++ mmengine/hooks/__init__.py
@@ -3,7 +3,9 @@ from .hook import Hook
 from .iter_timer_hook import IterTimerHook
 from .sampler_seed_hook import DistSamplerSeedHook
 from .param_scheduler_hook import ParamSchedulerHook
+from .optimizer_hook import OptimizerHook
 
 __all__ = [
-    'Hook', 'IterTimerHook', 'DistSamplerSeedHook', 'ParamSchedulerHook'
+    'Hook', 'IterTimerHook', 'DistSamplerSeedHook', 'ParamSchedulerHook',
+    'OptimizerHook'
 ]
