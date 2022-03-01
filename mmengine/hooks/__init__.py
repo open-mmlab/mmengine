2d3e91248c0dd98d26faf93283075f004dccb7ca
Yuan Liu <30762564+YuanLiuuuuuu@users.noreply.github.com>
2022-03-01 15:38:01 +0800

    [Feature]: Add sampler seed hook (#64)
    * [Feature]: Add sampler seed hook
    
    * [Fix]: Add call with to UT

--- mmengine/hooks/__init__.py
+++ mmengine/hooks/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .hook import Hook
 from .iter_timer_hook import IterTimerHook
+from .sampler_seed_hook import DistSamplerSeedHook
 
-__all__ = ['Hook', 'IterTimerHook']
+__all__ = ['Hook', 'IterTimerHook', 'DistSamplerSeedHook']
