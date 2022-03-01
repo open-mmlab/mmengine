6fe1e1c62602b4cb48d068784452ef4d8955efe7
Yifei Yang <2744335995@qq.com>
2022-03-01 11:20:50 +0800

    Update mmengine/hooks/empty_cache_hook.py
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>

--- mmengine/hooks/empty_cache_hook.py
+++ mmengine/hooks/empty_cache_hook.py
@@ -18,7 +18,7 @@ class EmptyCacheHook(Hook):
             False.
         after_epoch (bool): Whether to release cache after an epoch. Defaults to
             True.
-        after_iter (bool): Whether empty cache after an iteration. Defaults to
+        after_iter (bool): Whether to release cache after an iteration. Defaults to
             False.
     """
 
