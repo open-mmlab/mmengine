21e4c75636757e91b88f5dceb13ffe7ca0691d58
seuyou <3463423099@qq.com>
2022-03-01 17:54:06 +0800

    [Fix]: Fix lint

--- mmengine/hooks/__init__.py
+++ mmengine/hooks/__init__.py
@@ -1,11 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .empty_cache_hook import EmptyCacheHook
+from .checkpoint_hook import CheckpointHook
 from .hook import Hook
 from .iter_timer_hook import IterTimerHook
 from .optimizer_hook import OptimizerHook
 from .param_scheduler_hook import ParamSchedulerHook
 from .sampler_seed_hook import DistSamplerSeedHook
-from .checkpoint_hook import CheckpointHook
 
 __all__ = [
     'Hook', 'IterTimerHook', 'DistSamplerSeedHook', 'ParamSchedulerHook',
