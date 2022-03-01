63a3af4f8cf4de44d3bcab62360a5dcd0e33c219
Yuan Liu <30762564+YuanLiuuuuuu@users.noreply.github.com>
2022-03-01 17:42:15 +0800

    [Feature]: Add optimizer hook (#70)
    * [Feature]: Add optimizer hook
    
    * [Fix]: Update docstring
    
    * [Fix]: Add call with in UT

--- mmengine/hooks/__init__.py
+++ mmengine/hooks/__init__.py
@@ -1,9 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .hook import Hook
 from .iter_timer_hook import IterTimerHook
-from .sampler_seed_hook import DistSamplerSeedHook
+from .optimizer_hook import OptimizerHook
 from .param_scheduler_hook import ParamSchedulerHook
+from .sampler_seed_hook import DistSamplerSeedHook
 
 __all__ = [
-    'Hook', 'IterTimerHook', 'DistSamplerSeedHook', 'ParamSchedulerHook'
+    'Hook', 'IterTimerHook', 'DistSamplerSeedHook', 'ParamSchedulerHook',
+    'OptimizerHook'
 ]
