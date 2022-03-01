ab31e1936ea4d4905856cb21919cb6d65ecf3781
Yuan Liu <30762564+YuanLiuuuuuu@users.noreply.github.com>
2022-03-01 15:49:44 +0800

    [Feature]: Add param scheduler hook (#63)
    * [Feature]: Add param scheduler hook
    
    * [Fix]: update docstring and add assert_call to UT

--- mmengine/hooks/__init__.py
+++ mmengine/hooks/__init__.py
@@ -2,5 +2,8 @@
 from .hook import Hook
 from .iter_timer_hook import IterTimerHook
 from .sampler_seed_hook import DistSamplerSeedHook
+from .param_scheduler_hook import ParamSchedulerHook
 
-__all__ = ['Hook', 'IterTimerHook', 'DistSamplerSeedHook']
+__all__ = [
+    'Hook', 'IterTimerHook', 'DistSamplerSeedHook', 'ParamSchedulerHook'
+]
