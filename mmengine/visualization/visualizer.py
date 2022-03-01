9f1edc7fac4ca8131e4de149137d56af96d67e90
liukuikun <liukuikun@sensetime.com>
2022-03-01 21:40:58 +0800

    add draw featmap

--- mmengine/visualization/visualizer.py
+++ mmengine/visualization/visualizer.py
@@ -1,6 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-
-from typing import List, Optional, Union
+from typing import List, Optional, Sequence, Union
 
 import cv2
 import numpy as np
@@ -55,7 +54,6 @@ class Visualizer:
 
         return _register
 
-    # 但是如果不实现，那么各个下游库会出现大量重复代码
     def set_image(self, image):
         self._setup_fig(image)
 
@@ -76,10 +74,8 @@ class Visualizer:
         self.dpi = fig.get_dpi()
         # add a small 1e-2 to avoid precision lost due to matplotlib's
         # truncation (https://github.com/matplotlib/matplotlib/issues/15363)
-        fig.set_size_inches(
-            (self.width * self._scale + 1e-2) / self.dpi,
-            (self.height * self._scale + 1e-2) / self.dpi,
-        )
+        fig.set_size_inches((self.width * self._scale + 1e-2) / self.dpi,
+                            (self.height * self._scale + 1e-2) / self.dpi)
         self.canvas = FigureCanvasAgg(fig)
         # self.canvas = mpl.backends.backend_cairo.FigureCanvasCairo(fig)
         ax = fig.add_axes([0.0, 0.0, 1.0, 1.0])
@@ -194,10 +190,10 @@ class Visualizer:
                      is_filling: bool = True) -> 'Visualizer':
         assert isinstance(circle_coord, (np.ndarray, torch.Tensor))
         assert isinstance(radius, (np.ndarray, torch.Tensor))
-        assert circle_coord.shape[0] == radius.shape[0]
-        assert circle_coord.shape[-1] == 2
+
         if len(circle_coord.shape) == 1:
             circle_coord = circle_coord[None]
+        assert circle_coord.shape == (radius.shape[0], 2)
 
         circle_coord = circle_coord.tolist()
         radius = radius.tolist()
@@ -278,10 +274,96 @@ class Visualizer:
         self.ax.add_collection(polygon_collection)
         return self
 
-    def draw_binary_masks(self, binary_mask, **kwargs):
+    def draw_binary_masks(self,
+                          binary_mask: np.ndarray,
+                          color: Sequence[int],
+                          alpha: float = 0.5):
+        img = self.get_image()
+        assert img.shape[:2] == binary_mask.shape
+        binary_mask_complement = 1 - binary_mask
+        rgb = np.zeros_like(img)
+        rgb[...] = color
+        rgb = cv2.bitwise_and(rgb, rgb, mask=binary_mask)
+        img_complement = cv2.bitwise_and(img, img, mask=binary_mask_complement)
+        rgb = rgb + img_complement
+        img_add = cv2.addWeighted(img, alpha, rgb, 1 - alpha, 0)
+        self.ax.imshow(
+            img_add,
+            extent=(0, self.width, self.height, 0),
+            interpolation='nearest')
         return self
 
-    @classmethod
-    def draw_featmap(cls, tensor_chw, topk=-1):
+    @staticmethod
+    def draw_featmap(tensor_chw,
+                     img=None,
+                     mode='mean',
+                     topk=10,
+                     arrangement=(5, 2),
+                     alpha=0.3) -> np.ndarray:
 
-        pass
+        def concat_heatmap(feat_map, img=None, alpha=0.5):
+            norm_img = np.zeros(feat_map.shape)
+            norm_img = cv2.normalize(feat_map.numpy(), norm_img, 0, 255,
+                                     cv2.NORM_MINMAX)
+            norm_img = np.asarray(norm_img, dtype=np.uint8)
+            heat_img = cv2.applyColorMap(norm_img, cv2.COLORMAP_JET)
+            heat_img = cv2.cvtColor(heat_img, cv2.COLOR_BGR2RGB)
+            if img is not None:
+                heat_img = cv2.addWeighted(img, alpha, heat_img, 1 - alpha, 0)
+            return heat_img
+
+        tensor_chw = tensor_chw.detach().cpu()
+        if img is not None:
+            assert img.shape[:2] == tensor_chw.shape[1:]
+            if img.ndim == 2:
+                img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        if mode is not None:
+            assert mode in ['mean', 'max', 'min']
+            if mode == 'max':
+                feat_map = torch.max(tensor_chw, dim=0)
+            elif mode == 'min':
+                feat_map = torch.min(tensor_chw, dim=0)
+            elif mode == 'mean':
+                feat_map = torch.mean(tensor_chw, dim=0)
+            return concat_heatmap(feat_map, img, alpha)
+
+        if topk == -1:
+            tensor_chw_channel = tensor_chw.shape[0]
+            assert tensor_chw_channel in [1, 3]
+            if tensor_chw_channel == 1:
+                return concat_heatmap(tensor_chw, img, alpha)
+            else:
+                tensor_chw = tensor_chw.permute(1, 2, 0).numpy()
+                norm_img = np.zeros(tensor_chw.shape)
+                norm_img = cv2.normalize(tensor_chw, None, 0, 255,
+                                         cv2.NORM_MINMAX)
+                norm_img = np.asarray(norm_img, dtype=np.uint8)
+                heat_img = cv2.cvtColor(norm_img, cv2.COLOR_BGR2RGB)
+                if img is not None:
+                    heat_img = cv2.addWeighted(img, alpha, heat_img, 1 - alpha,
+                                               0)
+                return heat_img
+        else:
+            assert topk > 0
+            row, col = arrangement
+            height, width = tensor_chw.shape[1:]
+            sum_channel = torch.sum(tensor_chw, dim=(1, 2))
+            _, indices = torch.topk(sum_channel, topk)
+            topk_tensor = tensor_chw[indices]
+            fig = Figure(frameon=False)
+            dpi = fig.get_dpi()
+            fig.set_size_inches((width * col + 1e-2) / dpi,
+                                (height * row + 1e-2) / dpi)
+            canvas = FigureCanvasAgg(fig)
+            fig.subplots_adjust(wspace=0, hspace=0)
+            fig.tight_layout(h_pad=0, w_pad=0)
+
+            for i in range(topk):
+                axes = fig.add_subplot(row, col, i + 1)
+                axes.axis('off')
+                axes.imshow(concat_heatmap(topk_tensor[i], img, alpha))
+            s, (width, height) = canvas.print_to_buffer()
+            buffer = np.frombuffer(s, dtype='uint8')
+            img_rgba = buffer.reshape(height, width, 4)
+            rgb, alpha = np.split(img_rgba, [3], axis=2)
+            return rgb.astype('uint8')
