7ab85229ea1ad2def20d8014074c98c0ab369422
zhouzaida <zhouzaida@163.com>
2022-03-01 23:50:35 +0800

    minor fix

--- tests/test_dist/test_utils.py
+++ tests/test_dist/test_utils.py
@@ -1,36 +1,33 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from mmengine.dist import get_backend
-from mmengine.dist.utils import (barrier, get_dist_info, get_local_rank,
-                                 get_local_size, get_rank, get_world_size,
-                                 is_main_process)
+import mmengine.dist as dist
 
 
 def test_get_backend():
-    assert get_backend() is None
+    assert dist.get_backend() is None
 
 
 def test_get_world_size():
-    assert get_world_size() == 1
+    assert dist.get_world_size() == 1
 
 
 def test_get_rank():
-    assert get_rank() == 0
+    assert dist.get_rank() == 0
 
 
 def test_local_size():
-    assert get_local_size() == 1
+    assert dist.get_local_size() == 1
 
 
 def test_local_rank():
-    assert get_local_rank() == 0
+    assert dist.get_local_rank() == 0
 
 
 def test_get_dist_info():
-    assert get_dist_info() == (0, 1)
+    assert dist.get_dist_info() == (0, 1)
 
 
 def test_is_main_process():
-    assert is_main_process()
+    assert dist.is_main_process()
 
 
 def test_master_only():
@@ -38,4 +35,4 @@ def test_master_only():
 
 
 def test_barrier():
-    barrier()  # nothing is done
+    dist.barrier()  # nothing is done
