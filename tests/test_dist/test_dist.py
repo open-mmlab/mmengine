ddac3c1290deead8fcb6070113ba5a8f1c18fd53
zhouzaida <zhouzaida@163.com>
2022-03-01 22:32:07 +0800

    refactor unit tests

--- tests/test_dist/test_dist.py
+++ tests/test_dist/test_dist.py
@@ -12,28 +12,28 @@ def test_all_reduce():
     data = torch.arange(2, dtype=torch.int64)
     expected = torch.arange(2, dtype=torch.int64)
     dist.all_reduce(data)
-    torch.allclose(data, expected)
+    assert torch.allclose(data, expected)
 
 
 def test_all_gather():
     data = torch.arange(2, dtype=torch.int64)
     expected = torch.arange(2, dtype=torch.int64)
-    dist.all_gather(data)
-    torch.allclose(data[0], expected)
+    output = dist.all_gather(data)
+    assert torch.allclose(output[0], expected)
 
 
 def test_gather():
     data = torch.arange(2, dtype=torch.int64)
     expected = torch.arange(2, dtype=torch.int64)
-    dist.gather(data)
-    torch.allclose(data[0], expected)
+    output = dist.gather(data)
+    assert torch.allclose(output[0], expected)
 
 
 def test_broadcast():
     data = torch.arange(2, dtype=torch.int64)
     expected = torch.arange(2, dtype=torch.int64)
     dist.broadcast(data)
-    torch.allclose(data, expected)
+    assert torch.allclose(data, expected)
 
 
 @patch('numpy.random.randint', return_value=10)
@@ -41,7 +41,7 @@ def test_sync_random_seed(mock):
     assert sync_random_seed() == 10
 
 
-def test_broadcast_object_list(self):
+def test_broadcast_object_list():
     with pytest.raises(AssertionError):
         # input should be list of object
         dist.broadcast_object_list('foo')
@@ -67,7 +67,7 @@ def test_all_reduce_dict():
     }
     dist.all_reduce_dict(data)
     for key in data:
-        torch.allclose(data[key], expected[key])
+        assert torch.allclose(data[key], expected[key])
 
 
 def test_all_gather_object():
--- tests/test_dist/test_utils.py
+++ tests/test_dist/test_utils.py
@@ -5,31 +5,37 @@ from mmengine.dist.utils import (barrier, get_dist_info, get_local_rank,
                                  is_main_process)
 
 
-class TestNonDistributedEnv:
+def test_get_backend():
+    assert get_backend() is None
 
-    def test_get_backend(self):
-        assert get_backend() is None
 
-    def test_get_world_size(self):
-        assert get_world_size() == 1
+def test_get_world_size():
+    assert get_world_size() == 1
 
-    def test_get_rank(self):
-        assert get_rank() == 0
 
-    def test_local_size(self):
-        assert get_local_size() == 1
+def test_get_rank():
+    assert get_rank() == 0
 
-    def test_local_rank(self):
-        assert get_local_rank() == 0
 
-    def test_get_dist_info(self):
-        assert get_dist_info() == (0, 1)
+def test_local_size():
+    assert get_local_size() == 1
 
-    def test_is_main_process(self):
-        assert is_main_process()
 
-    def test_master_only(self):
-        pass
+def test_local_rank():
+    assert get_local_rank() == 0
 
-    def test_barrier(self):
-        barrier()  # nothing is done
+
+def test_get_dist_info():
+    assert get_dist_info() == (0, 1)
+
+
+def test_is_main_process():
+    assert is_main_process()
+
+
+def test_master_only():
+    pass
+
+
+def test_barrier():
+    barrier()  # nothing is done
