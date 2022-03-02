bdc523403004fb7701d569cc2d28c2df27dea40d
plyfager <2744335995@qq.com>
2022-03-02 11:23:56 +0800

    remove test condition

--- tests/test_hook/test_empty_cache_hook.py
+++ tests/test_hook/test_empty_cache_hook.py
@@ -1,18 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from unittest.mock import patch
-
 from mock import Mock
 
 from mmengine.hooks import EmptyCacheHook
 
 
-def mock(*args, **kwargs):
-    pass
-
-
 class TestEmptyCacheHook:
 
-    @patch('torch.cuda.empty_cache', mock)
     def test_emtpy_cache_hook(self):
         Hook = EmptyCacheHook(True, True, True)
         Runner = Mock()
