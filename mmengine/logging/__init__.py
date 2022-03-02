12b916cf6f57956ff158d85633773dec2c7bd035
Mashiro <57566630+HAOCHENYE@users.noreply.github.com>
2022-03-02 23:38:04 +0800

    [Feature] add base global access (#72)
    * add impl
    
    * add test
    
    * Fix as comment
    
    * Fix as comment
    
    * Fix as comment
    
    * Fix as comment

---
+++ mmengine/logging/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .base_global_accsessible import BaseGlobalAccessible, MetaGlobalAccessible
+
+__all__ = ['MetaGlobalAccessible', 'BaseGlobalAccessible']
