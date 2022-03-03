774d675d05cf3364055a17e66050347e806b7d85
HAOCHENYE <21724054@zju.edu.cn>
2022-03-03 21:33:28 +0800

    Fix scandir

--- mmengine/utils/path.py
+++ mmengine/utils/path.py
@@ -73,11 +73,11 @@ def scandir(dir_path, suffix=None, recursive=False, case_sensitive=True):
                 if suffix is not None:
                     if isinstance(suffix, tuple):
                         for suffix_element in suffix:
-                            if entry.name.endswith(suffix_element):
-                                yield _rel_path
+                            if _rel_path.endswith(suffix_element):
+                                yield rel_path
                     else:
-                        if entry.name.endswith(suffix):
-                            yield _rel_path
+                        if _rel_path.endswith(suffix):
+                            yield rel_path
             elif recursive and os.path.isdir(entry.path):
                 # scan recursively if entry.path is a directory
                 yield from _scandir(entry.path, suffix, recursive,
