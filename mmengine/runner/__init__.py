96b731040979021807c9fe50a5b9e15acb5958a3
zhouzaida <zhouzaida@163.com>
2022-03-06 15:58:06 +0800

    minor fix

--- mmengine/runner/__init__.py
+++ mmengine/runner/__init__.py
@@ -5,14 +5,7 @@ from .checkpoint import (CheckpointLoader, get_deprecated_model_names,
                          load_state_dict, save_checkpoint, weights_to_cpu)
 
 __all__ = [
-    'load_state_dict',
-    'get_torchvision_models',
-    'get_external_models',
-    'get_mmcls_models',
-    'get_deprecated_model_names',
-    'CheckpointLoader',
-    'load_checkpoint',
-    'weights_to_cpu',
-    'get_state_dict',
-    'save_checkpoint'
+    'load_state_dict', 'get_torchvision_models', 'get_external_models',
+    'get_mmcls_models', 'get_deprecated_model_names', 'CheckpointLoader',
+    'load_checkpoint', 'weights_to_cpu', 'get_state_dict', 'save_checkpoint'
 ]
