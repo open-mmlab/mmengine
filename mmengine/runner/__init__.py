8b0e74d327f07e97f58e46bbfb909a34add19a36
Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>
2022-03-06 22:05:07 +0800

    [Feature] Add checkpoint helper (#93)
    * [Feature] Add checkpoint helper
    
    * minor fix
    
    * add comments and fix format
    
    * add hub directory
    
    * add missing docstring

--- mmengine/runner/__init__.py
+++ mmengine/runner/__init__.py
@@ -1,1 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .checkpoint import (CheckpointLoader, get_deprecated_model_names,
+                         get_external_models, get_mmcls_models, get_state_dict,
+                         get_torchvision_models, load_checkpoint,
+                         load_state_dict, save_checkpoint, weights_to_cpu)
+
+__all__ = [
+    'load_state_dict', 'get_torchvision_models', 'get_external_models',
+    'get_mmcls_models', 'get_deprecated_model_names', 'CheckpointLoader',
+    'load_checkpoint', 'weights_to_cpu', 'get_state_dict', 'save_checkpoint'
+]
