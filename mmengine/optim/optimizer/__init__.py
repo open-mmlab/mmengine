49b7d0ce6fd4d2b86fae354884d13afa9445861c
RangiLyu <lyuchqi@gmail.com>
2022-03-08 16:05:29 +0800

    Support default_scope when building optimizer and evaluator. (#109)
    * Support default_scope when building optimizer and evaluator.
    
    * add docstring
    
    * fix
    
    * fix

--- mmengine/optim/optimizer/__init__.py
+++ mmengine/optim/optimizer/__init__.py
@@ -1,9 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .builder import (OPTIMIZER_CONSTRUCTORS, OPTIMIZERS, build_optimizer,
-                      build_optimizer_constructor)
+from .builder import OPTIMIZER_CONSTRUCTORS, OPTIMIZERS, build_optimizer
 from .default_constructor import DefaultOptimizerConstructor
 
 __all__ = [
     'OPTIMIZER_CONSTRUCTORS', 'OPTIMIZERS', 'DefaultOptimizerConstructor',
-    'build_optimizer', 'build_optimizer_constructor'
+    'build_optimizer'
 ]
--- mmengine/optim/optimizer/builder.py
+++ mmengine/optim/optimizer/builder.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import copy
 import inspect
-from typing import Callable, List
+from typing import List, Optional
 
 import torch
 import torch.nn as nn
@@ -10,6 +10,11 @@ from mmengine.registry import OPTIMIZER_CONSTRUCTORS, OPTIMIZERS
 
 
 def register_torch_optimizers() -> List[str]:
+    """Register optimizers in ``torch.optim`` to the ``OPTIMIZERS`` registry.
+
+    Returns:
+        List[str]: A list of registered optimizers' name.
+    """
     torch_optimizers = []
     for module_name in dir(torch.optim):
         if module_name.startswith('__'):
@@ -25,19 +30,35 @@ def register_torch_optimizers() -> List[str]:
 TORCH_OPTIMIZERS = register_torch_optimizers()
 
 
-def build_optimizer_constructor(cfg: dict) -> Callable:
-    return OPTIMIZER_CONSTRUCTORS.build(cfg)
+def build_optimizer(
+        model: nn.Module,
+        cfg: dict,
+        default_scope: Optional[str] = None) -> torch.optim.Optimizer:
+    """Build function of optimizer.
 
+    If ``constructor`` is set in the ``cfg``, this method will build an
+    optimizer constructor, and use optimizer constructor to build the
+    optimizer. If ``constructor`` is not set, the
+    ``DefaultOptimizerConstructor`` will be used by default.
 
-def build_optimizer(model: nn.Module, cfg: dict) -> torch.optim.Optimizer:
+    Args:
+        model (nn.Module): Model to be optimized.
+        cfg (dict): Config of optimizer and optimizer constructor.
+        default_scope (str, optional): The ``default_scope`` is used to
+            reset the current registry. Defaults to None.
+
+    Returns:
+        torch.optim.Optimizer: The built optimizer.
+    """
     optimizer_cfg = copy.deepcopy(cfg)
     constructor_type = optimizer_cfg.pop('constructor',
                                          'DefaultOptimizerConstructor')
     paramwise_cfg = optimizer_cfg.pop('paramwise_cfg', None)
-    optim_constructor = build_optimizer_constructor(
+    optim_constructor = OPTIMIZER_CONSTRUCTORS.build(
         dict(
             type=constructor_type,
             optimizer_cfg=optimizer_cfg,
-            paramwise_cfg=paramwise_cfg))
-    optimizer = optim_constructor(model)
+            paramwise_cfg=paramwise_cfg),
+        default_scope=default_scope)
+    optimizer = optim_constructor(model, default_scope=default_scope)
     return optimizer
