6f69039ca9abd3c26211c70c1a77d88454bd3e43
Mashiro <57566630+HAOCHENYE@users.noreply.github.com>
2022-03-08 16:10:30 +0800

    [Feature] Add LoggerHook (#77)
    * add logger hook
    
    * update
    
    * update
    
    * update test
    
    * update
    
    * update test
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Add logger hook
    
    * Fix pre-commit
    
    * Fix as comment
    
    * Fix as comment
    
    * Fix as comment
    
    * Fix as comment
    
    * Fix as comment
    
    * Fix bytes
    
    * update
    
    * Fix as comment
    
    * Fix as comment
    
    * Update runner
    
    * Fix as comment
    
    * Fix as comment
    
    * Fix as comment
    
    * Fix as comment

--- mmengine/hooks/__init__.py
+++ mmengine/hooks/__init__.py
@@ -3,6 +3,7 @@ from .checkpoint_hook import CheckpointHook
 from .empty_cache_hook import EmptyCacheHook
 from .hook import Hook
 from .iter_timer_hook import IterTimerHook
+from .logger_hook import LoggerHook
 from .optimizer_hook import OptimizerHook
 from .param_scheduler_hook import ParamSchedulerHook
 from .sampler_seed_hook import DistSamplerSeedHook
@@ -10,5 +11,6 @@ from .sync_buffer_hook import SyncBuffersHook
 
 __all__ = [
     'Hook', 'IterTimerHook', 'DistSamplerSeedHook', 'ParamSchedulerHook',
-    'OptimizerHook', 'SyncBuffersHook', 'EmptyCacheHook', 'CheckpointHook'
+    'OptimizerHook', 'SyncBuffersHook', 'EmptyCacheHook', 'CheckpointHook',
+    'LoggerHook'
 ]
