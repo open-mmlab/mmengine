9389a725d7c0f5839a580ef467e2a72f1340afe3
HAOCHENYE <21724054@zju.edu.cn>
2022-03-10 21:30:30 +0800

    Fix iter_timer_hook

--- mmengine/hooks/iter_timer_hook.py
+++ mmengine/hooks/iter_timer_hook.py
@@ -40,7 +40,8 @@ class IterTimerHook(Hook):
             mode (str): Current mode of runner. Defaults to 'train'.
         """
         # TODO: update for new logging system
-        runner.log_buffer.update({f'{mode}/data_time': time.time() - self.t})
+        runner.message_hub.update_log(
+            {f'{mode}/data_time': time.time() - self.t})
 
     def _after_iter(self,
                     runner,
@@ -61,5 +62,5 @@ class IterTimerHook(Hook):
         """
         # TODO: update for new logging system
 
-        runner.log_buffer.update({f'{mode}/time': time.time() - self.t})
+        runner.message_hub.update_log({f'{mode}/time': time.time() - self.t})
         self.t = time.time()
