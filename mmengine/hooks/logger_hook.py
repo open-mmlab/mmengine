7d18919a1a4c880423d6e9c2944811069b742862
HAOCHENYE <21724054@zju.edu.cn>
2022-03-10 22:00:01 +0800

    fix logger hook

--- mmengine/hooks/logger_hook.py
+++ mmengine/hooks/logger_hook.py
@@ -266,12 +266,13 @@ class LoggerHook(Hook):
             log_str = f'Epoch [{cur_epoch}]' \
                       f'[{cur_iter}/{len(runner.train_dataloader)}]\t'
         else:
-            log_str = f'Iter [{cur_iter}/{runner.max_iters}]\t'
+            log_str = f'Iter [{cur_iter}/{runner.train_loop.max_iters}]\t'
         log_str += f'{lr_momentum_str}, '
         # Calculate eta time.
         self.time_sec_tot += (tag['time'] * self.interval)
         time_sec_avg = self.time_sec_tot / (runner.iter - self.start_iter + 1)
-        eta_sec = time_sec_avg * (runner.max_iters - runner.iter - 1)
+        eta_sec = time_sec_avg * \
+                  (runner.train_loop.max_iters - runner.iter - 1)
         eta_str = str(datetime.timedelta(seconds=int(eta_sec)))
         log_str += f'eta: {eta_str}, '
         log_str += f'time: {tag["time"]:.3f}, ' \
