a0a42175b73470fa05a091048a289ce93cbfb159
zhouzaida <zhouzaida@163.com>
2022-03-20 11:31:36 +0800

    [Enhancement] Provide MultiProcessTestCase to test distributed related modules

--- mmengine/testing/__init__.py
+++ mmengine/testing/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .compare import assert_allclose
+from .distributed import MultiProcessTestCase
 
-__all__ = ['assert_allclose']
+__all__ = ['assert_allclose', 'MultiProcessTestCase']
