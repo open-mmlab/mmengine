f8ea231567cda75f487a9a08e1f8439c60310804
HAOCHENYE <21724054@zju.edu.cn>
2022-03-28 17:14:29 +0800

    add defaut scope

--- mmengine/registry/__init__.py
+++ mmengine/registry/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .default_scope import DefaultScope
 from .registry import Registry, build_from_cfg
 from .root import (DATA_SAMPLERS, DATASETS, EVALUATORS, HOOKS, LOOPS,
                    MODEL_WRAPPERS, MODELS, OPTIMIZER_CONSTRUCTORS, OPTIMIZERS,
@@ -9,5 +10,6 @@ __all__ = [
     'Registry', 'build_from_cfg', 'RUNNERS', 'RUNNER_CONSTRUCTORS', 'HOOKS',
     'DATASETS', 'DATA_SAMPLERS', 'TRANSFORMS', 'MODELS', 'WEIGHT_INITIALIZERS',
     'OPTIMIZERS', 'OPTIMIZER_CONSTRUCTORS', 'TASK_UTILS', 'PARAM_SCHEDULERS',
-    'EVALUATORS', 'MODEL_WRAPPERS', 'LOOPS', 'WRITERS', 'VISUALIZERS'
+    'EVALUATORS', 'MODEL_WRAPPERS', 'LOOPS', 'WRITERS', 'VISUALIZERS',
+    'DefaultScope'
 ]
---
+++ mmengine/registry/default_scope.py
@@ -0,0 +1,33 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmengine.utils.manager import ManagerMixin
+
+
+class DefaultScope(ManagerMixin):
+    """Scope of current task used to reset the current registry, which can be
+    accessed globally.
+
+    Args:
+        name (str): Name of default scope for global access. Defaults to ''.
+        scope_name (str): Scope of current task. Defaults to 'mmengine'.
+
+    Examples:
+        >>> from mmengine import MODELS
+        >>> # Define default scope in runner.
+        >>> DefaultScope.get_instance('mmdet')
+        >>> # Get default scope globally.
+        >>> scope = DefaultScope.get_instance('mmdet').scope
+        >>> # build model from cfg.
+        >>> model = MODELS.build(model_cfg, default_scope=scope)
+    """
+
+    def __init__(self, name, scope_name='mmengine'):
+        super().__init__(name)
+        self._scope_name = scope_name
+
+    @property
+    def scope_name(self) -> str:
+        """
+        Returns:
+            str: Get current scope.
+        """
+        return self._scope_name
