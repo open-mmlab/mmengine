da17f3dd62217b1be545020a5898aefb079b4a7a
liukuikun <641417025@qq.com>
2022-05-07 19:54:27 +0800

    remove num_classes property

--- mmengine/data/label_data.py
+++ mmengine/data/label_data.py
@@ -1,25 +1,20 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
-from typing import Union
+from typing import Optional, Union
 
 import torch
 
-from .base_data_element import BaseDataElement
+# from .base_data_element import BaseDataElement
+from mmengine.data import BaseDataElement
 
 
 class LabelData(BaseDataElement):
-    """Data structure for label-level annnotations or predictions."""
+    """Data structure for label-level annnotations or predictions.
 
-    @property
-    def num_classes(self) -> int:
-        """int: the number of classes.
-        """
-        return self._num_classes  # type: ignore
-
-    @num_classes.setter
-    def num_classes(self, value) -> None:
-        """set `num_classes`"""
-        self.set_field(value, '_num_classes', dtype=int, field_type='metainfo')
+    ``LabelData`` set ``item`` as default attributes. The type of ``item`` must
+    be ``str`` or ``torch.Tensor``. ``LabelData`` also support ``to_onehot``
+    and ``to_label`` when the type of ``item`` is ``torch.Tensor``.
+    """
 
     @property
     def item(self) -> Union[torch.TensorType, str]:
@@ -36,21 +31,36 @@ class LabelData(BaseDataElement):
         """delete `item`"""
         del self._item  # type: ignore
 
-    def to_onehot(self) -> None:
-        """convert `item` to onehot.
+    def to_onehot(self, num_classes: Optional[int] = None) -> None:
+        """convert `item` to onehot. If num_classes is None, `num_classes` must
+        in `LabelData.metainfo_keys()`
 
-        The `num_classes` must be set.
+        Args:
+            num_classes (Optional[int]): The number of classes.
         """
-        assert 'item' in self, 'please set `item`'
-        assert 'num_classes' in self, 'please set `num_classes`'
-        item = torch.zeros((self.num_classes, ), dtype=torch.int64)
+        assert 'item' in self and isinstance(self.item, torch.Tensor)
+        if num_classes is not None:
+            self.set_field(
+                num_classes, 'num_classes', dtype=int, field_type='metainfo')
+        assert 'num_classes' in self.metainfo_keys(), (
+            'Please set `num_classes` with '
+            '`.set_metainfo({num_classes=num_classes})` '
+            'or pass `num_classes` parameter')
+        item = torch.zeros(
+            (self.num_classes, ),  # type: ignore
+            dtype=torch.int64)
+        assert self.item.max().item() < self.num_classes  # type: ignore
         item[self.item] = 1
         self.item = item
 
     def to_label(self) -> None:
         """Convert `item` to label if its type is onehot."""
+        assert 'num_classes' in self.metainfo_keys(), (
+            'Please set `num_classes` with '
+            '`.set_metainfo({num_classes=num_classes})`')
+        assert 'item' in self and isinstance(self.item, torch.Tensor)
         if (isinstance(self.item, torch.Tensor) and self.item.ndim == 1
-                and self.item.shape[0] == self.num_classes
+                and self.item.shape[0] == self.num_classes  # type: ignore
                 and self.item.max().item() <= 1
                 and self.item.min().item() >= 0):
             self.item = self.item.nonzero().squeeze()
--- tests/test_data/test_label_data.py
+++ tests/test_data/test_label_data.py
@@ -10,7 +10,8 @@ from mmengine.data import LabelData
 class TestLabelData(TestCase):
 
     def test_set_data(self):
-        label_data = LabelData(num_classes=10, item=torch.IntTensor([5]))
+        label_data = LabelData(
+            item=torch.IntTensor([5]), metainfo=dict(num_classes=10))
         assert label_data.num_classes == 10
         assert label_data.item == torch.IntTensor([5])
         assert 'item' in label_data
@@ -27,34 +28,69 @@ class TestLabelData(TestCase):
         with pytest.raises(AssertionError):
             label_data.to_onehot()
 
+        # item is not onehot
+        with pytest.raises(AssertionError):
+            label_data.item = 'item'
+            label_data.set_metainfo(dict(num_classes=10))
+            label_data.to_onehot()
+
         # not set num_classes
         with pytest.raises(AssertionError):
+            label_data = LabelData()
             label_data.item = item
             label_data.to_onehot()
+
+        # num_classes not in metainfo
+        with pytest.raises(AssertionError):
+            label_data = LabelData()
+            label_data.item = item
+            label_data.num_classes = 10
+            label_data.to_onehot()
+
+        label_data = LabelData()
         label_data.item = item
-        label_data.num_classes = 10
+        label_data.set_metainfo(dict(num_classes=10))
         label_data.to_onehot()
         assert tuple(label_data.item.shape) == (10, )
 
+        label_data.item = item
+        label_data.to_onehot(11)
+        assert tuple(label_data.item.shape) == (11, )
+
         item = torch.arange(0, 9)
         label_data.item = item
-        label_data.to_onehot()
+        label_data.to_onehot(10)
         assert tuple(label_data.item.shape) == (10, )
 
     def test_to_label(self):
         label_data = LabelData()
         item = torch.tensor([1], dtype=torch.int64)
-        label_data.num_classes = 10
 
-        # not set num_classes
+        # without num_classes
+        with pytest.raises(AssertionError):
+            label_data.to_label()
+
+        # item is None
+        with pytest.raises(AssertionError):
+            label_data.set_metainfo(dict(num_classes=10))
+            label_data.to_label()
+
+        # item is not onehot
         with pytest.raises(
                 ValueError,
-                match='item is not onehot and can not convert to label'):
+                match='`item` is not onehot and can not convert to label'):
             label_data.item = item
+            label_data.set_metainfo(dict(num_classes=10))
+            label_data.to_label()
+
+        # item is not onehot
+        with pytest.raises(AssertionError):
+            label_data.item = 'item'
+            label_data.set_metainfo(dict(num_classes=10))
             label_data.to_label()
 
         item = torch.arange(0, 9)
         label_data.item = item
-        label_data.to_onehot()
+        label_data.to_onehot(10)
         label_data.to_label()
         assert (label_data.item == item).all()
