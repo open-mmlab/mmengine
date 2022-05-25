2c0699604bb2dfad2a73c52a6367fdab27b96fa8
PJLAB\huanghaian <1286304229@qq.com>
2022-05-25 18:50:42 +0800

    fix 'Runner' object has no attribute 'log_buffer'

--- mmengine/hooks/optimizer_hook.py
+++ mmengine/hooks/optimizer_hook.py
@@ -95,8 +95,12 @@ class OptimizerHook(Hook):
             grad_norm = self.clip_grads(runner.model.parameters())
             if grad_norm is not None:
                 # Add grad norm to the logger
-                runner.log_buffer.update({'grad_norm': float(grad_norm)},
-                                         runner.outputs['num_samples'])
+                runner.message_hub.update_scalars({
+                    'grad_norm':
+                    float(grad_norm),
+                    'count':
+                    runner.outputs['num_samples']
+                })
         runner.optimizer.step()
 
     def detect_anomalous_parameters(self, loss: torch.Tensor, runner) -> None:
