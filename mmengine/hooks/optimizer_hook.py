5911ee62a3637f04083e6f3dc6b01ea9e7991da8
PJLAB\huanghaian <1286304229@qq.com>
2022-05-26 10:25:14 +0800

    add train

--- mmengine/hooks/optimizer_hook.py
+++ mmengine/hooks/optimizer_hook.py
@@ -95,7 +95,8 @@ class OptimizerHook(Hook):
             grad_norm = self.clip_grads(runner.model.parameters())
             if grad_norm is not None:
                 # Add grad norm to the logger
-                runner.message_hub.update_scalar('grad_norm', float(grad_norm))
+                runner.message_hub.update_scalar('train/grad_norm',
+                                                 float(grad_norm))
         runner.optimizer.step()
 
     def detect_anomalous_parameters(self, loss: torch.Tensor, runner) -> None:
