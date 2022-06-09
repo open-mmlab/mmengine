11ff4e6cd78428cb4da0194097ba2eaad75833a3
imabackstabber <312276423@qq.com>
2022-06-09 21:44:15 +0800

    add assertion

--- mmengine/runner/runner.py
+++ mmengine/runner/runner.py
@@ -1213,6 +1213,8 @@ class Runner:
         elif isinstance(evaluator, dict):
             # if `metrics` in dict keys, it means to build customized evalutor
             if 'metrics' in evaluator:
+                assert 'type' in evaluator, 'expected customized evaluator' \
+                                    f' with key `type`,but got {evaluator}'
                 return EVALUATOR.build(evaluator)
             # otherwise, default evalutor will be built
             else:
