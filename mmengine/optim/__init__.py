217972da3b5fb9d4f0df3d65f0fe0cd9120e1d48
MeowZheng <meowzheng@outlook.com>
2022-06-11 19:35:52 +0800

    yapf disable

--- mmengine/optim/__init__.py
+++ mmengine/optim/__init__.py
@@ -2,14 +2,18 @@
 from .optimizer import (OPTIM_WRAPPER_CONSTRUCTORS, OPTIMIZERS,
                         AmpOptimWrapper, DefaultOptimWrapperConstructor,
                         OptimWrapper, OptimWrapperDict, build_optim_wrapper)
-from .scheduler import (
-    ConstantLR, ConstantMomentum, ConstantParamScheduler, CosineAnnealingLR,
-    CosineAnnealingMomentum, CosineAnnealingParamScheduler, ExponentialLR,
-    ExponentialMomentum, ExponentialParamScheduler, LinearLR, LinearMomentum,
-    LinearParamScheduler, MultiStepLR, MultiStepMomentum,
-    MultiStepParamScheduler, OneCycleLR, OneCycleParamScheduler, StepLR,
-    StepMomentum, StepParamScheduler, _ParamScheduler)
+# yapf: disable
+from .scheduler import (ConstantLR, ConstantMomentum, ConstantParamScheduler,
+                        CosineAnnealingLR, CosineAnnealingMomentum,
+                        CosineAnnealingParamScheduler, ExponentialLR,
+                        ExponentialMomentum, ExponentialParamScheduler,
+                        LinearLR, LinearMomentum, LinearParamScheduler,
+                        MultiStepLR, MultiStepMomentum,
+                        MultiStepParamScheduler, OneCycleLR,
+                        OneCycleParamScheduler, StepLR, StepMomentum,
+                        StepParamScheduler, _ParamScheduler)
 
+# yapf: enable
 __all__ = [
     'OPTIM_WRAPPER_CONSTRUCTORS', 'OPTIMIZERS', 'build_optim_wrapper',
     'DefaultOptimWrapperConstructor', 'ConstantLR', 'CosineAnnealingLR',
