bcab813242cf0be9ea98d8ffbe628c1fc2a477c2
Mashiro <57566630+HAOCHENYE@users.noreply.github.com>
2022-06-13 13:51:07 +0800

    [Feature] Add ModuleList Sequential and ModuleDict (#299)
    * add module list
    
    * add module list
    
    * fix docstring

--- mmengine/model/__init__.py
+++ mmengine/model/__init__.py
@@ -2,7 +2,7 @@
 from .averaged_model import (ExponentialMovingAverage, MomentumAnnealingEMA,
                              StochasticWeightAverage)
 from .base_model import BaseDataPreprocessor, BaseModel, ImgDataPreprocessor
-from .base_module import BaseModule
+from .base_module import BaseModule, ModuleDict, ModuleList, Sequential
 from .utils import detect_anomalous_params, merge_dict, stack_batch
 from .wrappers import (MMDistributedDataParallel,
                        MMSeparateDistributedDataParallel, is_model_wrapper)
@@ -12,5 +12,6 @@ __all__ = [
     'ExponentialMovingAverage', 'MomentumAnnealingEMA', 'BaseModel',
     'BaseDataPreprocessor', 'ImgDataPreprocessor',
     'MMSeparateDistributedDataParallel', 'BaseModule', 'stack_batch',
-    'merge_dict', 'detect_anomalous_params'
+    'merge_dict', 'detect_anomalous_params', 'ModuleList', 'ModuleDict',
+    'Sequential'
 ]
