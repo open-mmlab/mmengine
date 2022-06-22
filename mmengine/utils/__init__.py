03b8c16030e6f0cf621310394e904ac5b0706ed1
HAOCHENYE <21724054@zju.edu.cn>
2022-06-22 19:34:34 +0800

    first commit

--- mmengine/utils/__init__.py
+++ mmengine/utils/__init__.py
@@ -1,10 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .hub import load_url
 from .manager import ManagerMeta, ManagerMixin
-from .misc import (check_prerequisites, concat_list, deprecated_api_warning,
-                   has_batch_norm, has_method, import_modules_from_strings,
-                   is_list_of, is_method_overridden, is_seq_of, is_str,
-                   is_tuple_of, iter_cast, list_cast, mmcv_full_available,
+from .misc import (RemoveAssignFromAST, check_prerequisites, concat_list,
+                   deprecated_api_warning, has_batch_norm, has_method,
+                   import_modules_from_strings, is_list_of,
+                   is_method_overridden, is_seq_of, is_str, is_tuple_of,
+                   iter_cast, list_cast, mmcv_full_available,
                    requires_executable, requires_package, slice_list,
                    to_1tuple, to_2tuple, to_3tuple, to_4tuple, to_ntuple,
                    tuple_cast)
@@ -27,5 +28,5 @@ __all__ = [
     'is_method_overridden', 'has_method', 'mmcv_full_available',
     'digit_version', 'get_git_hash', 'TORCH_VERSION', 'load_url',
     'ManagerMeta', 'ManagerMixin', 'set_multi_processing', 'has_batch_norm',
-    'is_abs'
+    'is_abs', 'RemoveAssignFromAST'
 ]
--- mmengine/utils/misc.py
+++ mmengine/utils/misc.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import ast
 import collections.abc
 import functools
 import itertools
@@ -494,3 +495,21 @@ def has_batch_norm(model: nn.Module) -> bool:
         if has_batch_norm(m):
             return True
     return False
+
+
+class RemoveAssignFromAST(ast.NodeTransformer):
+    """Remove Assign node if the target's name match the key.
+
+    Args:
+        key (str): The target name of the Assign node.
+    """
+
+    def __init__(self, key):
+        self.key = key
+
+    def visit_Assign(self, node):
+        if (isinstance(node.targets[0], ast.Name)
+                and node.targets[0].id == self.key):
+            return None
+        else:
+            return node
