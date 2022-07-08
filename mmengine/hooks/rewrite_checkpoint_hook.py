9a1d3fd686d02dddf1a5df39870ce72af9f20b12
HAOCHENYE <21724054@zju.edu.cn>
2022-07-08 20:42:47 +0800

    rename, add docstring and unit test

---
+++ mmengine/hooks/rewrite_checkpoint_hook.py
@@ -0,0 +1,225 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import copy
+import re
+from typing import List
+
+from ..registry import HOOKS
+from ..utils import is_list_of
+from .hook import Hook
+
+
+@HOOKS.register_module()
+class RewriteCheckPointHook(Hook):
+    """A hook to rewrite key in checkpoint.
+
+    You can set ``applied_key`` to rewrite dictionary like instance saved in
+    checkpoint.
+
+    ``RewriteCheckPointHook`` has three mode to rewrite original checkpoint:
+
+    - remove: Removes specified keys in target dictionary saved in checkpoint.
+    - merge: Merge another state dictionary into the target dictionary.
+    - name_mapping: Maps the original key to the target one, and overwrites it.
+
+
+    Args:
+        applied_key (str): Target state dictionary saved in checkpoints, which
+            needs to be overwritten. Defaults to "state_dict".
+        removed_keys (List[str]): Keys need to be removed. Defaults to [].
+        name_mappings (List[dict]): A list of dictionary. Each dictionary has
+            two keys: ``src`` and ``dst``. ``src`` means the original key
+            prefix and ``src`` means the target key prefix, see more
+            information in examples. Defaults to [].
+        merged_state_dicts (List[str]): A list of checkpoint paths need to be
+            merged. Defaults to [].
+
+    Examples:
+        >>> import torch
+        >>> import torch.nn as nn
+
+        >>> from mmengine.hooks import RewriteCheckPointHook
+
+        >>> class SubModule(nn.Module):
+        >>>     def __init__(self) -> None:
+        >>>         super().__init__()
+        >>>         self.layer1 = nn.Linear(1, 1)
+        >>>         self.layer2 = nn.Linear(1, 1)
+
+        >>> class Model(nn.Module):
+        >>>     def __init__(self) -> None:
+        >>>         super().__init__()
+        >>>         self.layer1 = nn.Linear(1, 1)
+        >>>         self.layer2 = nn.Linear(1, 1)
+        >>>         self.submodule = SubModule()
+
+        >>> model = Model()
+        >>> model.state_dict().keys()
+        >>> # ['layer1.weight', 'layer1.bias', 'layer2.weight', 'layer2.bias',
+        >>> #  'submodule.layer1.weight', 'submodule.layer1.bias',
+        >>> #  'submodule.layer2.weight', 'submodule.layer2.bias']
+        >>>
+        >>> checkpoint = dict(state_dict=model.state_dict())
+        >>> hook = RewriteCheckPointHook(removed_keys='layer1')
+        >>> hook.after_load_checkpoint(None, checkpoint)
+        >>> checkpoint['state_dict'].keys()
+        >>> # ['layer2.weight', 'layer2.bias', 'submodule.layer1.weight',
+        >>> #  'submodule.layer1.bias', 'submodule.layer2.weight',
+        >>> #  'submodule.layer2.bias']
+        >>>
+        >>> checkpoint = dict(state_dict=model.state_dict())
+        >>> hook = RewriteCheckPointHook(removed_keys='submodule')
+        >>> hook.after_load_checkpoint(None, checkpoint)
+        >>> checkpoint['state_dict'].keys()
+        >>> # ['layer1.weight', 'layer1.bias', 'layer2.weight', 'layer2.bias']
+        >>>
+        >>> checkpoint = dict(state_dict=model.state_dict())
+        >>> hook = RewriteCheckPointHook(name_mappings=[dict(src='submodule', dst='module')])  # noqa: E501
+        >>> hook.after_load_checkpoint(None, checkpoint)
+        >>> checkpoint['state_dict'].keys()
+        >>> # ['layer1.weight', 'layer1.bias', 'layer2.weight', 'layer2.bias',
+        >>> #  'module.layer1.weight', 'module.layer1.bias',
+        >>> #  'module.layer2.weight', 'module.layer2.bias']
+        >>>
+        >>> checkpoint = dict(state_dict=model.state_dict())
+        >>> hook = RewriteCheckPointHook(
+        >>>     name_mappings=[dict(src='submodule', dst='module'),
+        >>>                 dict(src='layer1', dst='linear1')])
+        >>> hook.after_load_checkpoint(None, checkpoint)
+        >>> checkpoint['state_dict'].keys()
+        >>> # ['linear1.weight', 'linear1.bias', 'layer2.weight',
+        >>> #  'layer2.bias', 'module.layer1.weight', 'module.layer1.bias',
+        >>> #  'module.layer2.weight', 'module.layer2.bias']
+        >>>
+        >>> checkpoint = dict(state_dict=model.state_dict())
+        >>> merged_ckpt = dict(state_dict=nn.Conv2d(1, 1, 1).state_dict())
+        >>> torch.save(merged_ckpt, 'docs_demo.pth')
+        >>> hook = RewriteCheckPointHook(
+        >>>     merged_state_dicts=['docs_demo.pth'])
+        >>> hook.after_load_checkpoint(None, checkpoint)
+        >>> checkpoint['state_dict'].keys()
+        >>> # ['layer1.weight', 'layer1.bias', 'layer2.weight', 'layer2.bias',
+        >>> #  'submodule.layer1.weight', 'submodule.layer1.bias',
+        >>> #  'submodule.layer2.weight', 'submodule.layer2.bias',
+        >>> #  'weight', 'bias'
+    """
+
+    priority = 'HIGH'
+
+    def __init__(
+        self,
+        applied_key: str = 'state_dict',
+        removed_keys: List[str] = [],
+        name_mappings: List[dict] = [],
+        merged_state_dicts: List[str] = [],
+    ):
+        assert isinstance(applied_key, str), (
+            f'applied_key should be a string, but got {type(applied_key)}: '
+            f'{applied_key}')
+        if not isinstance(removed_keys, list):
+            removed_keys = [removed_keys]  # type: ignore
+
+        if not isinstance(name_mappings, list):
+            name_mappings = [name_mappings]  # type: ignore
+
+        if not isinstance(merged_state_dicts, list):
+            merged_state_dicts = [merged_state_dicts]  # type: ignore
+
+        assert is_list_of(removed_keys, str), (
+            'removed_keys should be a list instance or a single string, '
+            f'but got a {type(removed_keys)}: {removed_keys}')
+
+        assert is_list_of(merged_state_dicts, str), (
+            'merged_state_dicts should be a list or a single string, but got '
+            f'{type(merged_state_dicts)}: {merged_state_dicts}')
+
+        assert is_list_of(
+            name_mappings,
+            dict), ('name_mappings should be a list of dict a dict, but got '
+                    f'{type(name_mappings)}: {name_mappings}')
+
+        self.applied_key = applied_key
+        self.removed_keys = removed_keys
+        self.merged_state_dicts = merged_state_dicts
+        self.name_mappings = name_mappings
+
+    def after_load_checkpoint(self, runner, checkpoint: dict) -> None:
+        """Overwrites the key of corresponding status dictionary in checkpoint.
+
+        Args:
+            runner (Runner): Runner before training.
+            checkpoint (dict): loaded checkpoint.
+        """
+        new_state_dict: dict = dict()
+        state_dict: dict = checkpoint[self.applied_key]
+
+        for key in state_dict:
+            if self._should_remove(key):
+                continue
+            self._remapping_key(key, state_dict, new_state_dict)
+        self._merge_keys(new_state_dict)
+        checkpoint[self.applied_key] = new_state_dict
+
+    def _should_remove(self, key) -> bool:
+        """Returns whether to remove the key.
+
+        Args:
+            key (str): Key from original state dictionary.
+
+        Returns:
+            bool: Whether to remove the key.
+        """
+        matched_removed_keys = []
+        for removed_key in self.removed_keys:
+            if re.match(rf'{removed_key}(.*)', key) is not None:
+                matched_removed_keys.append(key)
+
+        # Each key in `state_dict` should only match one removed_keys at most.
+        if len(matched_removed_keys) == 0:
+            return False
+        elif len(matched_removed_keys) == 1:
+            return True
+        else:
+            raise ValueError(
+                f'removed_keys have a vague meaning, key: {key} '
+                f'matched with {matched_removed_keys} at the same time')
+
+    def _remapping_key(self, key: str, state_dict: dict,
+                       new_state_dict: dict) -> None:
+        """Maps the source key to the target key, and overwrites it.
+
+        Args:
+            key (str): Key from original state dictionary.
+            state_dict (dict): Original state dictionary.
+            new_state_dict (dict): New dictionary, of which key is remapped
+                from original dictionary.
+        """
+        matched_remapping_keys = []
+        for name_mapping in self.name_mappings:
+            src, dst = name_mapping['src'], name_mapping['dst']
+            if re.match(rf'{src}(.*)', key) is not None:
+                matched_remapping_keys.append((src, dst))
+
+        # Finds no mapped key,
+        if len(matched_remapping_keys) == 0:
+            src, dst = '', ''
+        # Each key should only match one `name_mapping`.
+        elif len(matched_remapping_keys) == 1:
+            src, dst = matched_remapping_keys[0]
+        else:
+            raise ValueError(
+                f'name_mappings have a vague meaning, key: {key} '
+                f'matched with {matched_remapping_keys} at the same time')
+        new_key = key.replace(src, dst)
+        new_state_dict[new_key] = copy.deepcopy(state_dict[key])
+
+    def _merge_keys(self, new_state_dict: dict) -> None:
+        """Merges state dictionary from other checkpoints.
+
+        Args:
+            new_state_dict (dict): New dictionary, of which key is remapped
+                from original dictionary.
+        """
+        from mmengine.runner.checkpoint import _load_checkpoint
+        for checkpoint_path in self.merged_state_dicts:
+            new_state_dict.update(
+                _load_checkpoint(checkpoint_path)['state_dict'])
---
+++ tests/test_hook/test_rewrite_checkpoint_hook.py
@@ -0,0 +1,286 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import copy
+import os.path as osp
+import tempfile
+from unittest import TestCase
+from unittest.mock import Mock
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+from mmengine.device import get_device
+from mmengine.hooks import RewriteCheckPointHook
+from mmengine.model import BaseModel
+from mmengine.optim import OptimWrapper
+from mmengine.registry import DATASETS
+from mmengine.runner import Runner
+
+
+class Model1(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(1, 1)
+        self.linear2 = nn.Linear(1, 1)
+
+    def forward(self, x):
+        return self.linear1(x) + self.linear2(x)
+
+
+class Model2(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.module1 = Model1()
+        self.module2 = Model1()
+
+    def forward(self, x):
+        return self.module1(x) + self.module2(x)
+
+
+class Model3(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.module1 = Model2()
+        self.module2 = Model2()
+
+    def forward(self, x):
+        return self.module1(x) + self.module2(x)
+
+
+class Model(BaseModel):
+
+    def __init__(self):
+        super().__init__()
+        self.module1 = Model1()
+        self.module2 = Model2()
+        self.module3 = Model3()
+
+    def forward(self, batch_inputs, labels, mode='tensor'):
+        if mode == 'loss':
+            loss = self.module1(self.module2(self.module3(batch_inputs)))
+            return dict(loss=loss)
+        else:
+            return []
+
+
+class ModelOld(BaseModel):
+
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(1, 1)
+        self.linear2 = nn.Linear(1, 1)
+        self.module2 = Model2()
+        self.module3 = Model3()
+
+    def forward(self, batch_inputs, labels, mode='tensor'):
+        if mode == 'loss':
+            loss = self.module2(self.linear1(self.linear2(batch_inputs)))
+            return dict(loss=loss)
+        else:
+            return []
+
+
+@DATASETS.register_module()
+class ToyDummyDataset(Dataset):
+    METAINFO = dict()  # type: ignore
+    data = torch.randn(12, 1)
+    label = torch.ones(12)
+
+    @property
+    def metainfo(self):
+        return self.METAINFO
+
+    def __len__(self):
+        return self.data.size(0)
+
+    def __getitem__(self, index):
+        return dict(inputs=self.data[index], data_sample=self.label[index])
+
+
+class TestModifyStateDictHook(TestCase):
+
+    def setUp(self):
+        self.temp_dir = tempfile.TemporaryDirectory()
+
+    def tearDown(self):
+        self.temp_dir.cleanup()
+
+    def test_init(self):
+        # Test with default constructor
+        hook = RewriteCheckPointHook()
+        self.assertEqual(hook.applied_key, 'state_dict')
+        self.assertEqual(hook.removed_keys, [])
+        self.assertEqual(hook.name_mappings, [])
+        self.assertEqual(hook.merged_state_dicts, [])
+
+        # Test with non-list arguments
+        hook = RewriteCheckPointHook(
+            applied_key='ema_state_dict',
+            removed_keys='module',
+            name_mappings=dict(src='layer', dst='linear'),
+            merged_state_dicts='path_to_ckpt',
+        )
+        self.assertEqual(hook.applied_key, 'ema_state_dict')
+        self.assertEqual(hook.removed_keys, ['module'])
+        self.assertEqual(hook.name_mappings, [dict(src='layer', dst='linear')])
+        self.assertEqual(hook.merged_state_dicts, ['path_to_ckpt'])
+
+        # Test with error format arguments.
+        with self.assertRaisesRegex(AssertionError,
+                                    'applied_key should be a string'):
+            RewriteCheckPointHook(applied_key=dict())
+
+        with self.assertRaisesRegex(AssertionError,
+                                    'removed_keys should be a list'):
+            RewriteCheckPointHook(removed_keys=dict())
+
+        with self.assertRaisesRegex(AssertionError,
+                                    'name_mappings should be a list'):
+            RewriteCheckPointHook(name_mappings='unknown')
+
+    def test_after_load_checkpoint(self):
+        model = Model()
+        state_dict = model.state_dict()
+        # module1.linear1
+        # module1.linear2
+        # module2.module1.linear1
+        # module2.module1.linear2
+        # module2.module2.linear1
+        # module3.module1.module2.linear2
+        # module3.module1.module1.linear1
+        # module3.module1.module1.linear2
+        # module3.module1.module2.linear1
+        # module3.module2.module2.linear2
+        # module3.module2.module2.linear2
+        # module3.module2.module1.linear1
+        # module3.module2.module1.linear2
+        # module3.module2.module2.linear1
+        # module3.module2.module2.linear2
+
+        # Test remove
+        # 1.1 remove specific key
+        self.assertIn('module3.module2.module2.linear1.weight', state_dict)
+        self.assertIn('module3.module2.module2.linear2.weight', state_dict)
+        ori_state_dict = copy.deepcopy(state_dict)
+        hook = RewriteCheckPointHook(removed_keys=[
+            'module3.module2.module2.linear1.weight',
+            'module3.module2.module2.linear2.weight'
+        ])
+        checkpoint = dict(state_dict=state_dict)
+        hook.after_load_checkpoint(Mock(), checkpoint)
+
+        self.assertNotIn('module3.module2.module2.linear1.weight',
+                         checkpoint['state_dict'])
+        self.assertNotIn('module3.module2.module2.linear2.weight',
+                         checkpoint['state_dict'])
+        # test state dict does not change.
+        self.assertEqual(state_dict, ori_state_dict)
+
+        # 1.2 Test remove keys with prefix
+        checkpoint = dict(state_dict=state_dict)
+        hook = RewriteCheckPointHook(removed_keys=['module2', 'module3'])
+        hook.after_load_checkpoint(Mock(), checkpoint)
+        target_state_dict = dict()
+        for key, value in state_dict.items():
+            if key.startswith('module1'):
+                target_state_dict[key] = value
+        self.assertEqual(target_state_dict, checkpoint['state_dict'])
+
+        # 1.3 Test overlapped removed keys
+        with self.assertRaisesRegex(ValueError,
+                                    'removed_keys have a vague meaning'):
+            checkpoint = dict(state_dict=state_dict)
+            hook = RewriteCheckPointHook(
+                removed_keys=['module2.module1', 'module2'])
+            hook.after_load_checkpoint(Mock(), checkpoint)
+
+        # 2. Test merge state dict
+        checkpoint = dict(state_dict=state_dict)
+        _merged_dict = {'a': 1, 'b': 2, 'module1.linear1.weight': 3}
+        merged_dict = dict(state_dict=_merged_dict)
+        torch.save(merged_dict, osp.join(self.temp_dir.name, 'tmp_ckpt'))
+        hook = RewriteCheckPointHook(
+            merged_state_dicts=[osp.join(self.temp_dir.name, 'tmp_ckpt')])
+        hook.after_load_checkpoint(Mock(), checkpoint)
+        self.assertEqual(checkpoint['state_dict']['a'], 1)
+        self.assertEqual(checkpoint['state_dict']['b'], 2)
+        self.assertEqual(checkpoint['state_dict']['module1.linear1.weight'], 3)
+
+        # Test remapping keys.
+        # 3.1 Test modify single key
+        checkpoint = dict(state_dict=state_dict)
+        self.assertNotIn('module3.module2.module2.linear3.weight', state_dict)
+        hook = RewriteCheckPointHook(name_mappings=[
+            dict(
+                src='module3.module2.module2.linear1.weight',
+                dst='module3.module2.module2.linear3.weight')
+        ])
+        hook.after_load_checkpoint(Mock(), checkpoint)
+        self.assertNotIn('module3.module2.module2.linear1.weight',
+                         checkpoint['state_dict'])
+        self.assertIn('module3.module2.module2.linear3.weight',
+                      checkpoint['state_dict'])
+
+        # 3.2 Test remapping prefix
+        checkpoint = dict(state_dict=state_dict)
+        self.assertNotIn('module3.linear2.weight', state_dict)
+        hook = RewriteCheckPointHook(
+            name_mappings=[dict(src='module3.module2.module2', dst='module3')])
+        hook.after_load_checkpoint(Mock(), checkpoint)
+        self.assertIn('module3.linear2.weight', checkpoint['state_dict'])
+
+        # 4 Test load state dict.
+        checkpoint = dict(state_dict=state_dict)
+        old_model = ModelOld()
+        hook = RewriteCheckPointHook(
+            name_mappings=[dict(src='module1.', dst='')])
+        hook.after_load_checkpoint(Mock(), checkpoint)
+        old_model.load_state_dict(checkpoint['state_dict'])
+
+    def test_with_runner(self):
+        device = get_device()
+        model = Model().to(device)
+        runner = Runner(
+            model=model,
+            train_dataloader=dict(
+                dataset=dict(type='ToyDummyDataset'),
+                sampler=dict(type='DefaultSampler', shuffle=True),
+                batch_size=3,
+                num_workers=0),
+            work_dir=self.temp_dir.name,
+            optim_wrapper=OptimWrapper(torch.optim.Adam(model.parameters())),
+            train_cfg=dict(by_epoch=True, max_epochs=2, val_interval=1),
+            default_hooks=dict(logger=None),
+            custom_hooks=[dict(type='EMAHook')],
+            experiment_name='test_rewrite_hook1')
+        runner.train()
+
+        old_model = ModelOld()
+        runner = Runner(
+            model=old_model,
+            train_dataloader=dict(
+                dataset=dict(type='ToyDummyDataset'),
+                sampler=dict(type='DefaultSampler', shuffle=True),
+                batch_size=3,
+                num_workers=0),
+            work_dir=self.temp_dir.name,
+            optim_wrapper=OptimWrapper(
+                torch.optim.Adam(old_model.parameters())),
+            train_cfg=dict(by_epoch=True, max_epochs=2, val_interval=1),
+            default_hooks=dict(logger=None),
+            custom_hooks=[
+                dict(type='EMAHook'),
+                dict(
+                    type='RewriteCheckPointHook',
+                    name_mappings=[dict(src='module1.', dst='')]),
+                dict(
+                    type='RewriteCheckPointHook',
+                    applied_key='ema_state_dict',
+                    name_mappings=[dict(src='module.module1.', dst='module.')])
+            ],
+            experiment_name='test_rewrite_hook2',
+            load_from=osp.join(self.temp_dir.name, 'epoch_2.pth'))
+        runner.train()
