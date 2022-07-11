bdd4bf578c969d6a18f9e22258662a6f58bff2c9
HAOCHENYE <21724054@zju.edu.cn>
2022-07-11 21:02:35 +0800

    fix docstring and type hint

--- mmengine/registry/build_function.py
+++ mmengine/registry/build_function.py
@@ -9,19 +9,19 @@ from .registry import Registry
 
 def build_from_cfg(
         cfg: Union[dict, ConfigDict, Config],
-        registry: 'Registry',
+        registry: Registry,
         default_args: Optional[Union[dict, ConfigDict, Config]] = None) -> Any:
     """Build a module from config dict when it is a class configuration, or
     call a function from config dict when it is a function configuration.
 
-    If the global variable default scope (:obj:`DefaultScope`) exists ,
+    If the global variable default scope (:obj:`DefaultScope`) exists,
     :meth:`build` will firstly get the responding registry and then call
     its own :meth:`build`.
 
-    At least one of the ``cfg`` and ``default_args`` contains the key "type"
-    which type should be either str or class. If they all contain it, the key
+    At least one of the ``cfg`` and ``default_args`` contains the key "type",
+    which should be either str or class. If they all contain it, the key
     in ``cfg`` will be used because ``cfg`` has a high priority than
-    ``default_args`` that means if a key exist in both of them, the value of
+    ``default_args`` that means if a key exists in both of them, the value of
     the key will be ``cfg[key]``. They will be merged first and the key "type"
     will be popped up and the remaining keys will be used as initialization
     arguments.
@@ -129,7 +129,7 @@ def build_from_cfg(
 
 
 def build_runner_from_cfg(cfg: Union[dict, ConfigDict, Config],
-                          registry: 'Registry') -> Any:
+                          registry: Registry) -> Any:
     """Build a Runner object.
     Examples:
         >>> from mmengine.registry import Registry, build_runner_from_cfg
