6705c9c77e653dbf2b7361a4f417c040eeedbd27
Mashiro <57566630+HAOCHENYE@users.noreply.github.com>
2022-07-14 16:18:25 +0800

    [Refactor] Refactor data flow: refine `data_preprocessor`. (#359)
    * refine data_preprocessor
    
    * remove unused BATCH_DATA alias
    
    * Fix type hints
    
    * rename move_data to cast_data

--- tests/test_model/test_base_model/test_data_preprocessor.py
+++ tests/test_model/test_base_model/test_data_preprocessor.py
@@ -16,16 +16,14 @@ class TestBaseDataPreprocessor(TestCase):
         self.assertEqual(base_data_preprocessor._device.type, 'cpu')
 
     def test_forward(self):
+        # Test cpu forward with list of data samples.
         base_data_preprocessor = BaseDataPreprocessor()
         input1 = torch.randn(1, 3, 5)
         input2 = torch.randn(1, 3, 5)
         label1 = torch.randn(1)
         label2 = torch.randn(1)
 
-        data = [
-            dict(inputs=input1, data_sample=label1),
-            dict(inputs=input2, data_sample=label2)
-        ]
+        data = dict(inputs=[input1, input2], data_sample=[label1, label2])
 
         batch_inputs, batch_labels = base_data_preprocessor(data)
         self.assertTrue(torch.is_floating_point(batch_inputs))
@@ -36,7 +34,22 @@ class TestBaseDataPreprocessor(TestCase):
         assert_allclose(label1, batch_labels[0])
         assert_allclose(label2, batch_labels[1])
 
+        # Test with tuple of batch inputs and batch data samples
+        data = dict(
+            inputs=torch.stack([input1, input2]), data_sample=[label1, label2])
+        batch_inputs, batch_labels = base_data_preprocessor(data)
+        self.assertTrue(torch.is_floating_point(batch_inputs))
+
+        # Test with invalid input type
+        with self.assertRaisesRegex(AssertionError, 'Data must have key'):
+            base_data_preprocessor(dict())
+
+        with self.assertRaisesRegex(TypeError, 'Inputs should be'):
+            base_data_preprocessor(dict(inputs=[dict()], data_sample=[]))
+
+        # Test cuda forward
         if torch.cuda.is_available():
+            # Test with list of data samples.
             base_data_preprocessor = base_data_preprocessor.cuda()
             batch_inputs, batch_labels = base_data_preprocessor(data)
             self.assertTrue(torch.is_floating_point(batch_inputs))
@@ -52,18 +65,24 @@ class TestBaseDataPreprocessor(TestCase):
             self.assertTrue(torch.is_floating_point(batch_inputs))
             self.assertEqual(batch_inputs.device.type, 'cuda')
 
+            # device of `base_data_preprocessor` is cuda, output should be
+            # cuda tensor.
+            self.assertEqual(batch_inputs.device.type, 'cuda')
+            self.assertEqual(batch_labels[0].device.type, 'cuda')
 
-class TestImgataPreprocessor(TestBaseDataPreprocessor):
+
+class TestImgDataPreprocessor(TestBaseDataPreprocessor):
 
     def test_init(self):
-        # initiate model without `preprocess_cfg`
+        # Initiate processor without arguments
         data_processor = ImgDataPreprocessor()
-        self.assertFalse(data_processor.channel_conversion)
+        self.assertFalse(data_processor._channel_conversion)
         self.assertFalse(hasattr(data_processor, 'mean'))
         self.assertFalse(hasattr(data_processor, 'std'))
         self.assertEqual(data_processor.pad_size_divisor, 1)
         assert_allclose(data_processor.pad_value, torch.tensor(0))
-        # initiate model with preprocess_cfg` and feat keys
+
+        # Initiate model with bgr2rgb, mean, std .etc..
         data_processor = ImgDataPreprocessor(
             bgr_to_rgb=True,
             mean=[0, 0, 0],
@@ -71,7 +90,7 @@ class TestImgataPreprocessor(TestBaseDataPreprocessor):
             pad_size_divisor=16,
             pad_value=10)
         self.assertTrue(data_processor._enable_normalize)
-        self.assertTrue(data_processor.channel_conversion, True)
+        self.assertTrue(data_processor._channel_conversion, True)
         assert_allclose(data_processor.mean,
                         torch.tensor([0, 0, 0]).view(-1, 1, 1))
         assert_allclose(data_processor.std,
@@ -113,10 +132,11 @@ class TestImgataPreprocessor(TestBaseDataPreprocessor):
         inputs2 = torch.randn(3, 15, 15)
         data_sample1 = InstanceData(bboxes=torch.randn(5, 4))
         data_sample2 = InstanceData(bboxes=torch.randn(5, 4))
-        data = [
-            dict(inputs=inputs1.clone(), data_sample=data_sample1.clone()),
-            dict(inputs=inputs2.clone(), data_sample=data_sample2.clone())
-        ]
+
+        data = dict(
+            inputs=[inputs1.clone(), inputs2.clone()],
+            data_sample=[data_sample1.clone(),
+                         data_sample2.clone()])
 
         std = torch.tensor([1, 2, 3]).view(-1, 1, 1)
         target_inputs1 = (inputs1.clone()[[2, 1, 0], ...] - 127.5) / std
@@ -159,22 +179,50 @@ class TestImgataPreprocessor(TestBaseDataPreprocessor):
         # Test gray image with 3 dim mean will raise error
         data_preprocessor = ImgDataPreprocessor(
             mean=(127.5, 127.5, 127.5), std=(127.5, 127.5, 127.5))
-        data = [
-            dict(inputs=torch.ones(10, 10)),
-            dict(inputs=torch.ones(10, 10))
-        ]
+        data = dict(
+            inputs=[torch.ones(10, 10), torch.ones(10, 10)], data_sample=None)
         with self.assertRaisesRegex(AssertionError,
                                     'If the mean has 3 values'):
             data_preprocessor(data)
 
-        data = [
-            dict(inputs=torch.ones(1, 10, 10)),
-            dict(inputs=torch.ones(1, 10, 10))
-        ]
+        data = dict(
+            inputs=[torch.ones(10, 10), torch.ones(10, 10)], data_sample=None)
         with self.assertRaisesRegex(AssertionError,
                                     'If the mean has 3 values'):
             data_preprocessor(data)
+
+        # Test stacked batch inputs and batch data samples
+        data_preprocessor = ImgDataPreprocessor(
+            mean=(127.5, 127.5, 127.5),
+            std=(127.5, 127.5, 127.5),
+            rgb_to_bgr=True,
+            pad_size_divisor=16)
+        _batch_inputs = torch.randn(2, 3, 10, 10)
+        _batch_labels = [torch.randn(1), torch.randn(1)]
+        data = dict(inputs=_batch_inputs, data_sample=_batch_labels)
+        batch_inputs, batch_labels = data_preprocessor(data)
+        target_batch_inputs = _batch_inputs[:, [2, 1, 0], ...]
+        target_batch_inputs = (target_batch_inputs - 127.5) / 127.5
+        target_batch_inputs = F.pad(target_batch_inputs, (0, 6, 0, 6), value=0)
+        self.assertEqual(batch_inputs.shape, torch.Size([2, 3, 16, 16]))
+        self.assertTrue(torch.is_floating_point(batch_inputs))
+        assert_allclose(target_batch_inputs, batch_inputs)
+
+        # Test batch inputs without convert channel order and pad
+        data_preprocessor = ImgDataPreprocessor(
+            mean=(127.5, 127.5, 127.5), std=(127.5, 127.5, 127.5))
+        _batch_inputs = torch.randn(2, 3, 10, 10)
+        _batch_labels = [torch.randn(1), torch.randn(1)]
+        data = dict(inputs=_batch_inputs, data_sample=_batch_labels)
+        batch_inputs, batch_labels = data_preprocessor(data)
+        target_batch_inputs = (_batch_inputs - 127.5) / 127.5
+        self.assertEqual(batch_inputs.shape, torch.Size([2, 3, 10, 10]))
+        self.assertTrue(torch.is_floating_point(batch_inputs))
+        assert_allclose(target_batch_inputs, batch_inputs)
 
         # Test empty `data_sample`
-        data = [dict(inputs=inputs1.clone()), dict(inputs=inputs2.clone())]
-        data_preprocessor(data, True)
+        data = dict(
+            inputs=[inputs1.clone(), inputs2.clone()], data_sample=None)
+        batch_inputs, batch_labels = data_preprocessor(data, True)
+        self.assertIsNone(batch_labels)
+        self.assertTrue(torch.is_floating_point(batch_inputs))
