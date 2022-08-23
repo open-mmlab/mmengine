0eb1ae3cd67fc7359faf1a01581548de4907c388
zhouzaida <zhouzaida@163.com>
2022-08-23 19:55:38 +0800

    add weight init in model/__init__.py

--- mmengine/model/__init__.py
+++ mmengine/model/__init__.py
@@ -7,6 +7,13 @@ from .base_model import BaseDataPreprocessor, BaseModel, ImgDataPreprocessor
 from .base_module import BaseModule, ModuleDict, ModuleList, Sequential
 from .utils import (detect_anomalous_params, merge_dict, revert_sync_batchnorm,
                     stack_batch)
+from .weight_init import (BaseInit, Caffe2XavierInit, ConstantInit,
+                          KaimingInit, NormalInit, PretrainedInit,
+                          TruncNormalInit, UniformInit, XavierInit,
+                          bias_init_with_prob, caffe2_xavier_init,
+                          constant_init, initialize, kaiming_init, normal_init,
+                          trunc_normal_init, uniform_init, update_init_info,
+                          xavier_init)
 from .wrappers import (MMDistributedDataParallel,
                        MMSeparateDistributedDataParallel, is_model_wrapper)
 
@@ -16,7 +23,12 @@ __all__ = [
     'MomentumAnnealingEMA', 'BaseModel', 'BaseDataPreprocessor',
     'ImgDataPreprocessor', 'MMSeparateDistributedDataParallel', 'BaseModule',
     'stack_batch', 'merge_dict', 'detect_anomalous_params', 'ModuleList',
-    'ModuleDict', 'Sequential', 'revert_sync_batchnorm'
+    'ModuleDict', 'Sequential', 'revert_sync_batchnorm', 'update_init_info',
+    'constant_init', 'xavier_init', 'normal_init', 'trunc_normal_init',
+    'uniform_init', 'kaiming_init', 'caffe2_xavier_init',
+    'bias_init_with_prob', 'BaseInit', 'ConstantInit', 'XavierInit',
+    'NormalInit', 'TruncNormalInit', 'UniformInit', 'KaimingInit',
+    'Caffe2XavierInit', 'PretrainedInit', 'initialize'
 ]
 
 if digit_version(TORCH_VERSION) >= digit_version('1.11.0'):
