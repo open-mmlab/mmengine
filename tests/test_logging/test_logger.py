68ceaf7ac036e7bdcecf7825b6ea7a13a436bfc6
zhouzaida <zhouzaida@163.com>
2022-08-23 17:45:21 +0800

    add a comment

--- tests/test_logging/test_logger.py
+++ tests/test_logging/test_logger.py
@@ -15,6 +15,8 @@ class TestLogger:
     stream_handler_regex_time = r'\d{2}/\d{2} \d{2}:\d{2}:\d{2}'
     file_handler_regex_time = r'\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}'
 
+    # Since `get_rank` has been imported in logger.py, it needs to mock
+    # `logger.get_rank`
     @patch('mmengine.logging.logger.get_rank', lambda: 0)
     def test_init_rank0(self, tmp_path):
         logger = MMLogger.get_instance('rank0.pkg1', log_level='INFO')
