971d079e0e952beecf9514501531f43c05d73044
HAOCHENYE <21724054@zju.edu.cn>
2022-08-25 19:03:19 +0800

    allign the previous version

--- mmengine/runner/utils.py
+++ mmengine/runner/utils.py
@@ -1,6 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import logging
-import os
 import random
 from typing import List, Optional, Tuple
 
@@ -68,9 +67,9 @@ def set_random_seed(seed: Optional[int] = None,
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
+    # torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
+    # os.environ['PYTHONHASHSEED'] = str(seed)
     if deterministic:
         if torch.backends.cudnn.benchmark:
             print_log(
