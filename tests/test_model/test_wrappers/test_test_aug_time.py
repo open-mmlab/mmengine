41344264ed21b8e6bcfe90a3cb6017520a07eb7b
HAOCHENYE <21724054@zju.edu.cn>
2022-10-09 16:26:51 +0800

    minor refine

--- tests/test_model/test_wrappers/test_test_aug_time.py
+++ tests/test_model/test_wrappers/test_test_aug_time.py
@@ -22,8 +22,10 @@ class ToyModel(BaseModel):
 class TestBaseTTAModel(TestCase):
 
     def setUp(self) -> None:
-        dict_dataset = [dict(inputs=[1, 2], data_samples=[3, 4])] * 10
-        tuple_dataset = [([1, 2], [3, 4])] * 10
+        dict_dataset = [
+            dict(inputs=[1, 2], data_samples=[3, 4]) for _ in range(10)
+        ]
+        tuple_dataset = [([1, 2], [3, 4]) for _ in range(10)]
         self.model = ToyModel()
         self.dict_dataloader = DataLoader(dict_dataset, batch_size=2)
         self.tuple_dataloader = DataLoader(tuple_dataset, batch_size=2)
