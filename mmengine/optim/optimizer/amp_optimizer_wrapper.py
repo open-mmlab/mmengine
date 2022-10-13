dfd83bfe4683514f2b23c5284ca5832300757501
wangjiangben-hw <111729245+wangjiangben-hw@users.noreply.github.com>
2022-10-13 14:54:43 +0800

    Update mmengine/optim/optimizer/amp_optimizer_wrapper.py
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>

--- mmengine/optim/optimizer/amp_optimizer_wrapper.py
+++ mmengine/optim/optimizer/amp_optimizer_wrapper.py
@@ -50,7 +50,7 @@ class AmpOptimWrapper(OptimWrapper):
         assert digit_version(TORCH_VERSION) >= digit_version('1.6.0'), (
             '`torch.cuda.amp` is only available when pytorch version >= 1.6')
         assert is_cuda_available() or is_npu_available(), (
-            '``AmpOptimizerWrapper`` is only available training on gpu')
+            '``AmpOptimizerWrapper`` is only available training on gpu or npu')
         super().__init__(**kwargs)
         self._scale_update_param = None
         if loss_scale == 'dynamic':
