9d63ea6278184b1318bd8904aff2addf6721c3c1
Hakjin Lee <nijkah@gmail.com>
2022-10-24 13:17:08 +0900

    Update mmengine/optim/optimizer/zero_optimizer.py
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>

--- mmengine/optim/optimizer/zero_optimizer.py
+++ mmengine/optim/optimizer/zero_optimizer.py
@@ -19,29 +19,33 @@ from .builder import OPTIMIZERS
 @OPTIMIZERS.register_module()
 class ZeroRedundancyOptimizer(_ZeroRedundancyOptimizer):
     """A wrapper class of :class:`ZeroRedundancyOptimizer` that gets a
-    optimizer type as string. This class wraps an arbitrary
-    :class:`optim.Optimizer.
-
-    <torch.optim.Optimizer>` and shards its states across ranks in the group as
-    described by ZeRO_. The local optimizer instance in each rank is only
-    responsible for updating approximately ``1 / world_size`` parameters and
-    hence only needs to keep ``1 / world_size`` optimizer states. After
-    parameters are updated locally, each rank will broadcast its parameters to
-    all other peers to keep all model replicas in the same state.
-    ``ZeroRedundancyOptimizer`` can be used in conjunction with
-    :class:`torch.nn.parallel.DistributedDataParallel` to reduce per-rank peak
+    optimizer type as string. 
+    
+    This class wraps an arbitrary :class:`torch.optim.Optimizer` and shards its
+    states across ranks in the group as described by ZeRO_. The local optimizer
+    instance in each rank is only responsible for updating approximately
+    ``1 / world_size`` parameters and hence only needs to keep ``1 / world_size``
+    optimizer states. After parameters are updated locally, each rank will
+    broadcast its parameters to all other peers to keep all model replicas
+    in the same state. ``ZeroRedundancyOptimizer`` can be used in conjunction
+    with :class:`torch.nn.parallel.DistributedDataParallel` to reduce per-rank peak
     memory consumption.
+
     ``ZeroRedundancyOptimizer`` uses a sorted-greedy algorithm to pack a number
     of parameters at each rank. Each parameter belongs to a single rank and is
     not divided among ranks. The partition is arbitrary and might not match the
     the parameter registration or usage order.
+
     Warnings:
         ``ZeroRedundancyOptimizer`` requires PyTorch >= 1.8.
+
     Args:
         params (``Iterable``): an ``Iterable`` of :class:`torch.Tensor` s
             or :class:`dict` s giving all parameters, which will be sharded
             across ranks.
         optimizer_type (str): the string of the local optimizer class.
+        
+    .. _ZeRO: https://arxiv.org/abs/1910.02054
     """
 
     def __init__(self, params, optimizer_type: str, **kwargs):
