376251961da47ea8254ab808ae5c51e1430f18dc
BigDong <yudongwang1226@gmail.com>
2022-11-01 17:06:02 +0800

    [Enhance] Add `ignore_keys` in ConcatDataset (#556)
    * [Fix] Fix ConcatDataset error in VOCDataset
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * add UT
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * Update mmengine/dataset/dataset_wrapper.py
    
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>
    
    * minor fix
    
    * Update mmengine/dataset/dataset_wrapper.py
    
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>
    
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>

--- mmengine/dataset/dataset_wrapper.py
+++ mmengine/dataset/dataset_wrapper.py
@@ -30,11 +30,15 @@ class ConcatDataset(_ConcatDataset):
             which will be concatenated.
         lazy_init (bool, optional): Whether to load annotation during
             instantiation. Defaults to False.
+        ignore_keys (List[str] or str): Ignore the keys that can be
+            unequal in `dataset.metainfo`. Defaults to None.
+            `New in version 0.3.0.`
     """
 
     def __init__(self,
                  datasets: Sequence[Union[BaseDataset, dict]],
-                 lazy_init: bool = False):
+                 lazy_init: bool = False,
+                 ignore_keys: Union[str, List[str], None] = None):
         self.datasets: List[BaseDataset] = []
         for i, dataset in enumerate(datasets):
             if isinstance(dataset, dict):
@@ -45,13 +49,33 @@ class ConcatDataset(_ConcatDataset):
                 raise TypeError(
                     'elements in datasets sequence should be config or '
                     f'`BaseDataset` instance, but got {type(dataset)}')
+        if ignore_keys is None:
+            self.ignore_keys = []
+        elif isinstance(ignore_keys, str):
+            self.ignore_keys = [ignore_keys]
+        elif isinstance(ignore_keys, list):
+            self.ignore_keys = ignore_keys
+        else:
+            raise TypeError('ignore_keys should be a list or str, '
+                            f'but got {type(ignore_keys)}')
+
+        meta_keys: set = set()
+        for dataset in self.datasets:
+            meta_keys |= dataset.metainfo.keys()
         # Only use metainfo of first dataset.
         self._metainfo = self.datasets[0].metainfo
         for i, dataset in enumerate(self.datasets, 1):
-            if self._metainfo != dataset.metainfo:
-                raise ValueError(
-                    f'The meta information of the {i}-th dataset does not '
-                    'match meta information of the first dataset')
+            for key in meta_keys:
+                if key in self.ignore_keys:
+                    continue
+                if key not in dataset.metainfo:
+                    raise ValueError(
+                        f'{key} does not in the meta information of '
+                        f'the {i}-th dataset')
+                if self._metainfo[key] != dataset.metainfo[key]:
+                    raise ValueError(
+                        f'The meta information of the {i}-th dataset does not '
+                        'match meta information of the first dataset')
 
         self._fully_initialized = False
         if not lazy_init:
