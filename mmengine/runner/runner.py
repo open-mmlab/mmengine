96aca0d6e443f46a906bffb9fe209d05a762263f
imAZhou <xiaoxiangyafei_1@163.com>
2022-11-13 15:17:26 +0800

    fix pre-commit

--- mmengine/runner/runner.py
+++ mmengine/runner/runner.py
@@ -5,8 +5,8 @@ import os
 import os.path as osp
 import pickle
 import platform
-import time
 import socket
+import time
 import warnings
 from collections import OrderedDict
 from functools import partial
@@ -37,18 +37,20 @@ from mmengine.registry import (DATA_SAMPLERS, DATASETS, EVALUATOR, HOOKS,
                                OPTIM_WRAPPERS, PARAM_SCHEDULERS, RUNNERS,
                                VISUALIZERS, DefaultScope,
                                count_registered_modules)
+from mmengine.runner.base_loop import BaseLoop
+from mmengine.runner.checkpoint import (_load_checkpoint,
+                                        _load_checkpoint_to_model,
+                                        find_latest_checkpoint, get_state_dict,
+                                        save_checkpoint, weights_to_cpu)
+from mmengine.runner.log_processor import LogProcessor
+from mmengine.runner.loops import (EpochBasedTrainLoop, IterBasedTrainLoop,
+                                   TestLoop, ValLoop)
+from mmengine.runner.priority import Priority, get_priority
+from mmengine.runner.utils import set_random_seed
 from mmengine.utils import digit_version, get_git_hash, is_seq_of
 from mmengine.utils.dl_utils import (TORCH_VERSION, collect_env,
                                      set_multi_processing)
 from mmengine.visualization import Visualizer
-from mmengine.runner.base_loop import BaseLoop
-from mmengine.runner.checkpoint import (_load_checkpoint, _load_checkpoint_to_model,
-                         find_latest_checkpoint, get_state_dict,
-                         save_checkpoint, weights_to_cpu)
-from mmengine.runner.log_processor import LogProcessor
-from mmengine.runner.loops import EpochBasedTrainLoop, IterBasedTrainLoop, TestLoop, ValLoop
-from mmengine.runner.priority import Priority, get_priority
-from mmengine.runner.utils import set_random_seed
 
 ConfigType = Union[Dict, Config, ConfigDict]
 ParamSchedulerType = Union[List[_ParamScheduler], Dict[str,
