bb45dac9560241f8a56cc4fe5931dc4e0847c466
HAOCHENYE <21724054@zju.edu.cn>
2022-12-09 12:49:03 +0800

    test EmptyCacheHook with runner

--- tests/test_hooks/test_empty_cache_hook.py
+++ tests/test_hooks/test_empty_cache_hook.py
@@ -1,14 +1,46 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from unittest.mock import Mock
+from unittest.mock import patch
 
-from mmengine.hooks import EmptyCacheHook
+from mmengine.testing import RunnerTestCase
 
 
-class TestEmptyCacheHook:
+class TestEmptyCacheHook(RunnerTestCase):
 
-    def test_emtpy_cache_hook(self):
-        hook = EmptyCacheHook(True, True, True)
-        runner = Mock()
-        hook._after_iter(runner, 0)
-        hook._before_epoch(runner)
-        hook._after_epoch(runner)
+    def test_with_runner(self):
+        with patch('torch.cuda.empty_cache') as mock_empty_cache:
+            cfg = self.epoch_based_cfg
+            cfg.custom_hooks = [dict(type='EmptyCacheHook')]
+            cfg.train_cfg.val_interval = 1e6  # disable val during train
+            runner = self.build_runner(cfg)
+
+            runner.train()
+            runner.test()
+            runner.val()
+
+            target_called_times = runner.max_epochs + 2
+            self.assertEqual(mock_empty_cache.call_count, target_called_times)
+
+        with patch('torch.cuda.empty_cache') as mock_empty_cache:
+            cfg.custom_hooks = [dict(type='EmptyCacheHook', before_epoch=True)]
+            runner = self.build_runner(cfg)
+            runner.train()
+            runner.val()
+            runner.test()
+            target_called_times = runner.max_epochs * 2 + 4
+            self.assertEqual(mock_empty_cache.call_count, target_called_times)
+
+        with patch('torch.cuda.empty_cache') as mock_empty_cache:
+            cfg.custom_hooks = [
+                dict(
+                    type='EmptyCacheHook', after_iter=True, before_epoch=True)
+            ]
+            runner = self.build_runner(cfg)
+            runner.train()
+            runner.val()
+            runner.test()
+            target_called_times = \
+                runner.max_epochs * 2 + 4 + \
+                len(runner.train_dataloader) * runner.max_epochs + \
+                len(runner.val_dataloader) + \
+                len(runner.test_dataloader)
+            self.assertEqual(mock_empty_cache.call_count, target_called_times)
