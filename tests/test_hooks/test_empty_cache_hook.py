cceda7584d6c4b9b24e5855b5b1a5a05feac2417
HAOCHENYE <21724054@zju.edu.cn>
2022-12-09 16:53:58 +0800

    Add coments

--- tests/test_hooks/test_empty_cache_hook.py
+++ tests/test_hooks/test_empty_cache_hook.py
@@ -10,22 +10,33 @@ class TestEmptyCacheHook(RunnerTestCase):
         with patch('torch.cuda.empty_cache') as mock_empty_cache:
             cfg = self.epoch_based_cfg
             cfg.custom_hooks = [dict(type='EmptyCacheHook')]
-            cfg.train_cfg.val_interval = 1e6  # disable val during train
+            cfg.train_cfg.val_interval = 1e6  # disable validation during training  # noqa: E501
             runner = self.build_runner(cfg)
 
             runner.train()
             runner.test()
             runner.val()
 
+            # Call `torch.cuda.empty_cache` after each epoch:
+            #   runner.train: `max_epochs` times.
+            #   runner.val: `1` time.
+            #   runner.test: `1` time.
             target_called_times = runner.max_epochs + 2
             self.assertEqual(mock_empty_cache.call_count, target_called_times)
 
         with patch('torch.cuda.empty_cache') as mock_empty_cache:
             cfg.custom_hooks = [dict(type='EmptyCacheHook', before_epoch=True)]
             runner = self.build_runner(cfg)
+
             runner.train()
             runner.val()
             runner.test()
+
+            # Call `torch.cuda.empty_cache` after/before each epoch:
+            #   runner.train: `max_epochs*2` times.
+            #   runner.val: `1*2` times.
+            #   runner.test: `1*2` times.
+
             target_called_times = runner.max_epochs * 2 + 4
             self.assertEqual(mock_empty_cache.call_count, target_called_times)
 
@@ -35,9 +46,17 @@ class TestEmptyCacheHook(RunnerTestCase):
                     type='EmptyCacheHook', after_iter=True, before_epoch=True)
             ]
             runner = self.build_runner(cfg)
+
             runner.train()
             runner.val()
             runner.test()
+
+            # Call `torch.cuda.empty_cache` after/before each epoch,
+            # after each iteration:
+            #   runner.train: `max_epochs*2 + len(dataloader)*max_epochs` times.  # noqa: E501
+            #   runner.val: `1*2 + len(val_dataloader)` times.
+            #   runner.test: `1*2 + len(val_dataloader)` times.
+
             target_called_times = \
                 runner.max_epochs * 2 + 4 + \
                 len(runner.train_dataloader) * runner.max_epochs + \
