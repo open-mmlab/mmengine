b0d046a635b410761f0f0b2a98eae085c9d45fcf
HAOCHENYE <21724054@zju.edu.cn>
2022-12-10 21:03:46 +0800

    Refactor unit test of param_schemeduler hook

--- tests/test_hooks/test_param_scheduler_hook.py
+++ tests/test_hooks/test_param_scheduler_hook.py
@@ -1,18 +1,18 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import copy
 from unittest.mock import Mock
 
-import pytest
-
 from mmengine.hooks import ParamSchedulerHook
+from mmengine.testing import RunnerTestCase
 
 
-class TestParamSchedulerHook:
+class TestParamSchedulerHook(RunnerTestCase):
     error_msg = ('runner.param_schedulers should be list of ParamScheduler or '
                  'a dict containing list of ParamScheduler')
 
     def test_after_iter(self):
         # runner.param_schedulers should be a list or dict
-        with pytest.raises(TypeError, match=self.error_msg):
+        with self.assertRaisesRegex(TypeError, self.error_msg):
             hook = ParamSchedulerHook()
             runner = Mock()
             scheduler = Mock()
@@ -46,7 +46,7 @@ class TestParamSchedulerHook:
 
     def test_after_epoch(self):
         # runner.param_schedulers should be a list or dict
-        with pytest.raises(TypeError, match=self.error_msg):
+        with self.assertRaisesRegex(TypeError, self.error_msg):
             hook = ParamSchedulerHook()
             runner = Mock()
             scheduler = Mock()
@@ -77,3 +77,60 @@ class TestParamSchedulerHook:
         hook.after_train_epoch(runner)
         scheduler1.step.assert_called()
         scheduler2.step.assert_called()
+
+    def test_with_runner(self):
+        cfg = copy.deepcopy(self.epoch_based_cfg)
+        cfg.train_cfg.max_epochs = 3
+        cfg.param_scheduler = [
+            dict(
+                type='ConstantLR',
+                factor=0.5,
+                begin=0,
+            ),
+            dict(
+                type='ConstantLR',
+                factor=0.5,
+                begin=1,
+            )
+        ]
+        init_lr = cfg.optim_wrapper.optimizer.lr
+        runner = self.build_runner(cfg)
+        runner.train()
+
+        # Length of train log is 4
+        # Learning rate of the first epoch is init_lr*0.5
+        # Learning rate of the second epoch is init_lr*0.5*0.5
+        # Learning rate of the last epoch will be reset to 0.1
+        train_lr = list(runner.message_hub.get_scalar('train/lr')._log_history)
+        target_lr = [init_lr * 0.5] * 4 + \
+                    [init_lr * 0.5 * 0.5] * 4 + \
+                    [0.1] * 4
+        self.assertListEqual(train_lr, target_lr)
+
+        cfg = copy.deepcopy(self.iter_based_cfg)
+        cfg.param_scheduler = [
+            dict(
+                type='ConstantLR',
+                factor=0.5,
+                begin=0,
+                by_epoch=False,
+            ),
+            dict(
+                type='ConstantLR',
+                factor=0.5,
+                begin=4,
+                by_epoch=False,
+            )
+        ]
+
+        init_lr = cfg.optim_wrapper.optimizer.lr
+        runner = self.build_runner(cfg)
+        runner.train()
+
+        # Learning rate of 1-4 iteration is init_lr*0.5
+        # Learning rate of 5-11 iteration is init_lr*0.5*0.5
+        train_lr = list(runner.message_hub.get_scalar('train/lr')._log_history)
+        target_lr = [init_lr * 0.5] * 4 + \
+                    [init_lr * 0.5 * 0.5] * 7 + \
+                    [0.1]
+        self.assertListEqual(train_lr, target_lr)
