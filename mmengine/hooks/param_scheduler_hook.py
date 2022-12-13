68085fe1c82f59d984da195c05118607764a8cb5
LEFTeyes <lzy06100809@163.com>
2022-12-13 17:28:19 +0800

    fix ParamSchedulerHook after_val_epoch execute without train_loop and param_schedulers built

--- mmengine/hooks/param_scheduler_hook.py
+++ mmengine/hooks/param_scheduler_hook.py
@@ -1,8 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from typing import Dict, Optional, Union
 
-from mmengine.optim import ReduceOnPlateauParamScheduler
+from mmengine.optim import ReduceOnPlateauParamScheduler, _ParamScheduler
 from mmengine.registry import HOOKS
+from mmengine.runner import BaseLoop
 from .hook import Hook
 
 DATA_BATCH = Optional[Union[dict, tuple, list]]
@@ -33,14 +34,14 @@ class ParamSchedulerHook(Hook):
                 keep ``data_batch`` here.
         """
 
+        if runner.param_schedulers is None:
+            return
+
         def step(param_schedulers):
             assert isinstance(param_schedulers, list)
             for scheduler in param_schedulers:
                 if not scheduler.by_epoch:
                     scheduler.step()
-
-        if runner.param_schedulers is None:
-            return
 
         if isinstance(runner.param_schedulers, list):
             step(runner.param_schedulers)
@@ -60,14 +61,14 @@ class ParamSchedulerHook(Hook):
             runner (Runner): The runner of the training process.
         """
 
+        if runner.param_schedulers is None:
+            return
+
         def step(param_schedulers):
             assert isinstance(param_schedulers, list)
             for scheduler in param_schedulers:
                 if scheduler.by_epoch:
                     scheduler.step()
-
-        if runner.param_schedulers is None:
-            return
 
         if isinstance(runner.param_schedulers, list):
             step(runner.param_schedulers)
@@ -84,6 +85,12 @@ class ParamSchedulerHook(Hook):
                         runner,
                         metrics: Optional[Dict[str, float]] = None) -> None:
 
+        if runner.param_schedulers is None:
+            return
+
+        if not self._should_after_val_epoch(runner):
+            return
+
         def step(param_schedulers):
             assert isinstance(param_schedulers, list)
             for scheduler in param_schedulers:
@@ -92,9 +99,6 @@ class ParamSchedulerHook(Hook):
                                                == 'val'):
                         scheduler.metric = metrics[scheduler.monitor_key]
                         scheduler.step()
-
-        if runner.param_schedulers is None:
-            return
 
         if isinstance(runner.param_schedulers, list):
             step(runner.param_schedulers)
@@ -106,3 +110,31 @@ class ParamSchedulerHook(Hook):
                 'runner.param_schedulers should be list of ParamScheduler or '
                 'a dict containing list of ParamScheduler, '
                 f'but got {runner.param_schedulers}')
+
+    def _should_after_val_epoch(self, runner) -> bool:
+        """Check whether train_loop and param_schedulers is built."""
+        self._should: bool
+
+        if hasattr(self, '_should'):
+            # to save check time
+            return self._should
+
+        # Check train_loop is built
+        # Need to skip building train_loop when call runner.train_loop,
+        # so use runner._train_loop. This is a hacky approach.
+        if not isinstance(runner._train_loop, BaseLoop):
+            self._should = False
+            return self._should
+
+        # Check param_schedulers is built
+        # copy from runner.scale_lr method
+        def _is_built(schedulers) -> bool:
+            if isinstance(schedulers, dict):
+                return False if 'type' in schedulers else any(
+                    _is_built(s) for s in schedulers.values())
+            if isinstance(schedulers, list):
+                return any(_is_built(s) for s in schedulers)
+            return isinstance(schedulers, _ParamScheduler)
+
+        self._should = _is_built(runner.param_schedulers)
+        return self._should
