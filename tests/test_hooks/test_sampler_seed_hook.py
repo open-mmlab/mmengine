358e4ba36e20c3735f23c3fce27014e4af133d7c
Mashiro <57566630+HAOCHENYE@users.noreply.github.com>
2022-12-21 22:36:33 +0800

    [Refactor] Use a real runner to test DistSamplerSeedHook (#812)
    * Refactor unit test of DistSamplerSeedHook
    
    * rename the case
    
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>

--- tests/test_hooks/test_sampler_seed_hook.py
+++ tests/test_hooks/test_sampler_seed_hook.py
@@ -1,29 +1,28 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from unittest.mock import Mock
+from unittest.mock import MagicMock
 
 from mmengine.hooks import DistSamplerSeedHook
+from mmengine.testing import RunnerTestCase
 
 
-class TestDistSamplerSeedHook:
+class TestDistSamplerSeedHook(RunnerTestCase):
 
-    def test_before_epoch(self):
+    def test_before_train_epoch(self):
 
         hook = DistSamplerSeedHook()
         # Test dataset sampler
-        runner = Mock()
+        runner = MagicMock()
         runner.epoch = 1
-        runner.train_loop.dataloader = Mock()
-        runner.train_loop.dataloader.sampler = Mock()
-        runner.train_loop.dataloader.sampler.set_epoch = Mock()
         hook.before_train_epoch(runner)
         runner.train_loop.dataloader.sampler.set_epoch.assert_called()
         # Test batch sampler
-        runner = Mock()
-        runner.train_loop.dataloader = Mock()
-        runner.train_loop.dataloader.sampler = Mock(spec_set=True)
-        runner.train_loop.dataloader.batch_sampler = Mock()
-        runner.train_loop.dataloader.batch_sampler.sampler = Mock()
-        runner.train_loop.dataloader.batch_sampler.sampler.set_epoch = Mock()
+        runner.train_loop.dataloader = MagicMock(spec_set=['batch_sampler'])
         hook.before_train_epoch(runner)
         runner.train_loop.dataloader.\
             batch_sampler.sampler.set_epoch.assert_called()
+
+    def test_with_runner(self):
+        cfg = self.epoch_based_cfg
+        cfg.custom_hooks = [dict(type='DistSamplerSeedHook')]
+        runner = self.build_runner(cfg)
+        runner.train()
