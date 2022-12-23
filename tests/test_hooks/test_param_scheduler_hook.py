f93ea752bcc02ace4a07b4ba5980eb793a8cb885
LEFTeyes <lzy06100809@163.com>
2022-12-23 20:36:42 +0800

    [Fix] modified ParamSchedulerHook test

--- tests/test_hooks/test_param_scheduler_hook.py
+++ tests/test_hooks/test_param_scheduler_hook.py
@@ -4,13 +4,15 @@ from unittest.mock import Mock
 import pytest
 
 from mmengine.hooks import ParamSchedulerHook
+from mmengine.optim import _ParamScheduler
+from mmengine.runner import BaseLoop
 
 
 class TestParamSchedulerHook:
     error_msg = ('runner.param_schedulers should be list of ParamScheduler or '
                  'a dict containing list of ParamScheduler')
 
-    def test_after_iter(self):
+    def test_after_train_iter(self):
         # runner.param_schedulers should be a list or dict
         with pytest.raises(TypeError, match=self.error_msg):
             hook = ParamSchedulerHook()
@@ -42,9 +44,10 @@ class TestParamSchedulerHook:
         runner.param_schedulers = dict(key1=[scheduler1], key2=[scheduler2])
         hook.after_train_epoch(runner)
         hook.after_train_iter(runner, 0)
+        scheduler1.step.assert_called()
         scheduler2.step.assert_called()
 
-    def test_after_epoch(self):
+    def test_after_train_epoch(self):
         # runner.param_schedulers should be a list or dict
         with pytest.raises(TypeError, match=self.error_msg):
             hook = ParamSchedulerHook()
@@ -53,7 +56,7 @@ class TestParamSchedulerHook:
             scheduler.step = Mock()
             scheduler.by_epoch = True
             runner.param_schedulers = scheduler
-            hook.after_train_iter(runner, 0)
+            hook.after_train_epoch(runner)
             scheduler.step.assert_called()
 
         # runner.param_schedulers is a list of schedulers
@@ -74,6 +77,96 @@ class TestParamSchedulerHook:
         scheduler2.step = Mock()
         scheduler2.by_epoch = True
         runner.param_schedulers = dict(key1=[scheduler1], key2=[scheduler2])
+        hook.after_train_iter(runner, 0)
         hook.after_train_epoch(runner)
         scheduler1.step.assert_called()
         scheduler2.step.assert_called()
+
+    def test_after_val_epoch(self):
+        metrics = dict(loss=1.0)
+
+        # mock super BaseLoop class
+        class MockBaseLoop(BaseLoop):
+
+            def __init__(self):
+                pass
+
+            def run(self):
+                pass
+
+        # mock super _ParamScheduler class
+        class MockParamScheduler(_ParamScheduler):
+
+            def __init__(self):
+                pass
+
+            def _get_value(self):
+                pass
+
+        # runner.param_schedulers is None
+        hook = ParamSchedulerHook()
+        runner = Mock()
+        runner.param_schedulers = None
+        assert hook.after_val_epoch(runner, metrics) is None
+
+        # metrics is None
+        scheduler = Mock()
+        runner.param_schedulers = [scheduler]
+        assert hook.after_val_epoch(runner, None) is None
+
+        # runner._train_loop is not built
+        hook = ParamSchedulerHook()  # release self._should
+        runner._train_loop = None
+        assert hook._should_after_val_epoch(runner) is False
+
+        # runner.param_schedulers is not built
+        hook = ParamSchedulerHook()  # release self._should
+        runner._train_loop = MockBaseLoop()
+        runner.param_schedulers = dict(key1=[None], key2=[None])
+        assert hook._should_after_val_epoch(runner) is False
+
+        # all is built
+        hook = ParamSchedulerHook()  # release self._should
+        runner._train_loop = MockBaseLoop()
+        scheduler = MockParamScheduler()
+        runner.param_schedulers = [scheduler]
+        assert hook._should_after_val_epoch(runner) is True
+
+        # runner.param_schedulers should be a list or dict
+        with pytest.raises(TypeError, match=self.error_msg):
+            hook = ParamSchedulerHook()
+            hook._should = True
+            runner = Mock()
+            scheduler = Mock()
+            scheduler.step = Mock()
+            scheduler.by_epoch = True
+            scheduler.need_val_args = True
+            runner.param_schedulers = scheduler
+            hook.after_val_epoch(runner, metrics)
+            scheduler.step.assert_called_with(metrics)
+
+        # runner.param_schedulers is a list of schedulers
+        hook = ParamSchedulerHook()
+        hook._should = True
+        runner = Mock()
+        scheduler = Mock()
+        scheduler.step = Mock()
+        scheduler.by_epoch = True
+        scheduler.need_val_args = True
+        runner.param_schedulers = [scheduler]
+        hook.after_val_epoch(runner, metrics)
+        scheduler.step.assert_called_with(metrics)
+
+        # runner.param_schedulers is a dict containing list of schedulers
+        scheduler1 = Mock()
+        scheduler1.step = Mock()
+        scheduler1.by_epoch = True
+        scheduler1.need_val_args = True
+        scheduler2 = Mock()
+        scheduler2.step = Mock()
+        scheduler2.by_epoch = True
+        scheduler2.need_val_args = True
+        runner.param_schedulers = dict(key1=[scheduler1], key2=[scheduler2])
+        hook.after_val_epoch(runner, metrics)
+        scheduler1.step.assert_called_with(metrics)
+        scheduler2.step.assert_called_with(metrics)
