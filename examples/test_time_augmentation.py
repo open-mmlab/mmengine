d0ca8f58d3e6889b4d7dc7f6858af37a98c1d2c5
HAOCHENYE <21724054@zju.edu.cn>
2022-12-26 11:37:55 +0800

    remove chinese comment

--- examples/test_time_augmentation.py
+++ examples/test_time_augmentation.py
@@ -13,13 +13,10 @@ class ClsTTAModel(BaseTTAModel):
         merged_data_samples = []
         for data_samples in data_samples_list:
             merged_data_samples.append(self._merge_single_sample(data_samples))
-        # 返回 batch 增强后的结果
         return merged_data_samples
 
     def _merge_single_sample(self, data_samples):
-        # data_samples 中每一个元素都是同一张图片增强后的结果，类型也是一个 list
         merged_data_sample = data_samples[0].new()
-        # 融合结果
         merged_score = sum(data_sample.pred_label.score
                            for data_sample in data_samples) / len(data_samples)
         merged_data_sample.set_pred_score(merged_score)
@@ -29,7 +26,6 @@ class ClsTTAModel(BaseTTAModel):
 if __name__ == '__main__':
     cfg = get_config('mmcls::resnet/resnet50_8xb16_cifar10.py')
     cfg.work_dir = 'work_dirs/resnet50_8xb16_cifar10'
-    # 将模型替换成 TTAModel
     cfg.model = dict(type='ClsTTAModel', module=cfg.model)
     test_pipeline = deepcopy(cfg.test_dataloader.dataset.pipeline)
     flip_tta = dict(
@@ -41,7 +37,7 @@ if __name__ == '__main__':
             ],
             [test_pipeline[-1]],
         ])
-    # 将最后一个 transform 替换成 TestTimeAug
+    # Replace the last transform with `TestTimeAug`
     cfg.test_dataloader.dataset.pipeline[-1] = flip_tta
     cfg.load_from = 'https://download.openmmlab.com/mmclassification/v0' \
                     '/resnet/resnet50_b16x8_cifar10_20210528-f54bfad9.pth'
