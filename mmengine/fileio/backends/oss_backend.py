871ece75450ffbd1d4fd186cd10cb396f66d8245
goog <33951338+liuxianyi@users.noreply.github.com>
2022-12-27 21:27:39 +0800

    Update mmengine/fileio/backends/oss_backend.py
    Co-authored-by: Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>

--- mmengine/fileio/backends/oss_backend.py
+++ mmengine/fileio/backends/oss_backend.py
@@ -329,7 +329,7 @@ class OSSBackend(BaseStorageBackend):
             >>> backend.copyfile_from_local(src, dst)
             'oss://endpoint/bucket/dir/file'
         """
-        dst = self._format_path(self._map_path(self._map_path(dst)))
+        dst = self._format_path(self._map_path(dst))
         src = self._map_path(src)
         if dst.endswith('/'):
             dst = os.path.join(dst, src.split('/')[-1])
