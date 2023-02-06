7a825d04b60bdbdeb839e0589f9dcfae86312534
Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>
2023-02-06 15:11:34 +0800

    Update apex_optimizer_wrapper.py

--- mmengine/optim/optimizer/apex_optimizer_wrapper.py
+++ mmengine/optim/optimizer/apex_optimizer_wrapper.py
@@ -23,67 +23,72 @@ class ApexOptimWrapper(OptimWrapper):
     precision training based on apex.amp.
 
     ``ApexOptimWrapper`` provides a unified interface with
-    ``OptimWrapper``, so ``ApexOptimWrapper`` can be used in the same way
-    as ``OptimWrapper``.
+    ``OptimWrapper``, so it can be used in the same way as ``OptimWrapper``.
 
     Warning:
         ``ApexOptimWrapper`` requires `nvidia apex <https://github.com/NVIDIA/apex>`_
 
     Args:
-        opt_level (str, default="O1"): Pure or mixed precision
-            optimization level. Accepted values are "O0", "O1", "O2",
-            and "O3".
-        loss_scale (float or str, default=None): If passed as
-            a string, must be a string representing a number,
-            e.g., "128.0", or the string "dynamic".
-        enabled (bool, default=True): If False, renders all Amp calls no-ops,
-            so your script should run as if Amp were not present.
-        cast_model_type (torch.dtype, default=None): Model's parameters and
-            buffers to the desired type.
-        patch_torch_functions (bool, default=None): Patch all Torch functions
+        opt_level (str): Pure or mixed precision optimization level. Accepted
+            values are "O0", "O1", "O2", and "O3". Defaults to "O1".
+        loss_scale (float or str, optional): If passed as a string, must be a
+            string representing a number, e.g., "128.0", or the string
+            "dynamic". Defaults to "dynamic".
+        enabled (bool): If False, renders all Amp calls no-ops, so your script
+            should run as if Amp were not present. Defaults to True.
+        cast_model_type (torch.dtype, optional): Model's parameters and
+            buffers to the desired type. Defaults to None.
+        patch_torch_functions (bool, optional): Patch all Torch functions
             and Tensor methods to perform Tensor Core-friendly ops like GEMMs
-            and convolutions in FP16,
-            and any ops that benefit from FP32 precision in FP32.
-        keep_batchnorm_fp32 (bool or str, default=None): To enhance precision
+            and convolutions in FP16, and any ops that benefit from FP32
+            precision in FP32. Defaults to None.
+        keep_batchnorm_fp32 (bool or str, optional): To enhance precision
             and enable cudnn batchnorm (which improves performance),
             it's often beneficial to keep batchnorm weights in FP32
             even if the rest of the model is FP16.
             If passed as a string, must be the string "True" or "False".
-        master_weights (bool, default=None): Maintain FP32 master weights to
+            Defaults to None.
+        master_weights (bool, optional): Maintain FP32 master weights to
             accompany any FP16 model weights. FP32 master weights are stepped
             by the optimizer to enhance precision and capture small gradients.
-        cast_model_outputs (torch.dtype, default=None): Option to ensure that
+            Defaults to None.
+        cast_model_outputs (torch.dtype, optional): Option to ensure that
             the outputs of your model(s) are always cast to a particular type
-            regardless of ``opt_level``.
-        num_losses (int, default=1): Option to tell Amp in advance how many
-            losses/backward passes you plan to use.
-        verbosity (int, default=1): Set to 0 to suppress Amp-related output.
-        min_loss_scale (float, default=None): Sets a floor for the loss scale
+            regardless of ``opt_level``. Defaults to None.
+        num_losses (int): Option to tell Amp in advance how many
+            losses/backward passes you plan to use. Defaults to 1.
+        verbosity (int): Set to 0 to suppress Amp-related output.
+            Defaults to 1.
+        min_loss_scale (float, optional): Sets a floor for the loss scale
             values that can be chosen by dynamic loss scaling.
             The default value of None means that no floor is imposed.
             If dynamic loss scaling is not used, `min_loss_scale` is ignored.
-        max_loss_scale (float, default=2.**24): Sets a ceiling for the
-            loss scale values that can be chosen by dynamic loss scaling.
-            If dynamic loss scaling is not used, `max_loss_scale` is ignored.
+            Defaults to None.
+        max_loss_scale (float, optional): Sets a ceiling for the loss scale
+            values that can be chosen by dynamic loss scaling. If dynamic
+            loss scaling is not used, `max_loss_scale` is ignored.
+            Defaults to 2.**24.
         **kwargs: Keyword arguments passed to OptimWrapper.
 
     Note:
         If you use ``IterBasedRunner`` and enable gradient accumulation,
         the original `max_iters` should be multiplied by
         ``accumulative_counts``.
+        
+    `New in version 0.6.0.`
     """  # noqa: E501
 
     def __init__(self,
                  opt_level: str = 'O1',
-                 loss_scale: Union[float, str] = 'dynamic',
+                 loss_scale: Union[float, str, None] = 'dynamic',
                  enabled: Optional[bool] = True,
                  cast_model_type: Optional[torch.dtype] = None,
                  patch_torch_functions: Optional[bool] = None,
-                 keep_batchnorm_fp32: Optional[Union[bool, str]] = None,
+                 keep_batchnorm_fp32: Union[bool, str, None] = None,
                  master_weights: Optional[bool] = None,
                  cast_model_outputs: Optional[torch.dtype] = None,
-                 num_losses: Optional[int] = 1,
-                 verbosity: Optional[int] = 1,
+                 num_losses: int = 1,
+                 verbosity: int = 1,
                  min_loss_scale: Optional[float] = None,
                  max_loss_scale: Optional[float] = 2.**24,
                  **kwargs):
