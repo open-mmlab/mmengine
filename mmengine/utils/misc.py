50b94810e8d3867782a8aa38b2d7379f36dd838f
HAOCHENYE <21724054@zju.edu.cn>
2023-03-07 02:24:05 +0800

    Fix as comment

--- mmengine/utils/misc.py
+++ mmengine/utils/misc.py
@@ -225,18 +225,17 @@ def apply_to(data: Any, expr: Callable, apply_func: Callable):
     `np.ndarray` to `Tensor`. You can use the following code:
 
     Examples:
-
-    >>> from mmengine.utils import apply_to
-    >>> import numpy as np
-    >>> import torch
-    >>> data = dict(array=[np.array(1)]) # {'array': [array(1)]}
-    >>> result = apply_to(data, lambda x: isinstance(x, np.ndarray), lambda x: torch.from_numpy(x))
-    >>> print(result) # {'array': [tensor(1)]}
+        >>> from mmengine.utils import apply_to
+        >>> import numpy as np
+        >>> import torch
+        >>> data = dict(array=[np.array(1)]) # {'array': [array(1)]}
+        >>> result = apply_to(data, lambda x: isinstance(x, np.ndarray), lambda x: torch.from_numpy(x))
+        >>> print(result) # {'array': [tensor(1)]}
 
     Args:
         data (Any): Data to be applied.
         expr (Callable): Expression to tell which data should be applied with
-            the function.
+            the function. It should return a boolean.
         apply_func (Callable): Function applied to data.
 
     Returns:
@@ -249,14 +248,14 @@ def apply_to(data: Any, expr: Callable, apply_func: Callable):
 
     def _apply(data, expr, apply_func):
         if isinstance(data, dict):
-            return {key: apply_to(data[key], expr, apply_func) for key in data}
+            return {key: _apply(data[key], expr, apply_func) for key in data}
         elif isinstance(data, (str, bytes)) or data is None:
             return data
         elif isinstance(data, tuple) and hasattr(data, '_fields'):
             # namedtuple
-            return type(data)(*(apply_to(sample, expr, apply_func) for sample in data))  # type: ignore  # noqa: E501  # yapf:disable
+            return type(data)(*(_apply(sample, expr, apply_func) for sample in data))  # type: ignore  # noqa: E501  # yapf:disable
         elif isinstance(data, (tuple, list)):
-            return type(data)(apply_to(sample, expr, apply_func) for sample in data)  # type: ignore  # noqa: E501  # yapf:disable
+            return type(data)(_apply(sample, expr, apply_func) for sample in data)  # type: ignore  # noqa: E501  # yapf:disable
         elif expr(data):
             return apply_func(data)
         else:
