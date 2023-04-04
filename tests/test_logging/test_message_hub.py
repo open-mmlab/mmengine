6eed41ddd856f5f85f58ba0d05b653307b82604b
enkilee <luhputu0815@gmail.com>
2023-04-04 23:21:46 +0800

    fix

--- tests/test_logging/test_message_hub.py
+++ tests/test_logging/test_message_hub.py
@@ -82,8 +82,6 @@ class TestMessageHub:
 
     def test_get_runtime(self):
         message_hub = MessageHub.get_instance('mmengine')
-        with pytest.raises(KeyError):
-            message_hub.get_info('unknown')
         recorded_dict = dict(a=1, b=2)
         message_hub.update_info('test_value', recorded_dict)
         assert message_hub.get_info('test_value') == recorded_dict
@@ -185,11 +183,6 @@ class TestMessageHub:
         message_hub.update_info('tensor', [1, 2, 3], resumed=False)
         obj = pickle.dumps(message_hub)
         instance = pickle.loads(obj)
-
-        with pytest.raises(KeyError):
-            instance.get_info('feat')
-        with pytest.raises(KeyError):
-            instance.get_info('lr')
 
         instance.get_info('iter')
         instance.get_scalar('loss')
