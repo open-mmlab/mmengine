ff9320bb7e46b464aa0462fa87f86ca5cf0149ad
huangqiyuan <huangqiyuan@cambricon.com>
2023-04-13 17:48:33 +0800

    fix lint errors

--- mmengine/optim/optimizer/amp_optimizer_wrapper.py
+++ mmengine/optim/optimizer/amp_optimizer_wrapper.py
@@ -5,7 +5,8 @@ from typing import Union
 import torch
 import torch.nn as nn
 
-from mmengine.device import is_cuda_available, is_npu_available, is_mlu_available
+from mmengine.device import (is_cuda_available, is_mlu_available,
+                             is_npu_available)
 from mmengine.registry import OPTIM_WRAPPERS
 from mmengine.utils import digit_version
 from mmengine.utils.dl_utils import TORCH_VERSION
@@ -67,8 +68,9 @@ class AmpOptimWrapper(OptimWrapper):
                  **kwargs):
         assert digit_version(TORCH_VERSION) >= digit_version('1.6.0'), (
             '`torch.cuda.amp` is only available when pytorch version >= 1.6')
-        assert is_cuda_available() or is_npu_available() or is_mlu_available(), (
-            '``AmpOptimizerWrapper`` is only available training on gpu, npu or mlu')
+        assert is_cuda_available() or is_npu_available() or is_mlu_available(
+        ), ('``AmpOptimizerWrapper`` is only available training '
+            'on gpu, npu or mlu')
         super().__init__(**kwargs)
         self._scale_update_param = None
         if loss_scale == 'dynamic':
