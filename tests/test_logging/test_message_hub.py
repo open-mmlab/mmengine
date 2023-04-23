f8cc43cf90b1ee3297bf0aa15ee5fa4ac0ef5d90
Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>
2023-04-23 17:23:22 +0800

    Apply suggestions from code review

--- tests/test_logging/test_message_hub.py
+++ tests/test_logging/test_message_hub.py
@@ -82,8 +82,7 @@ class TestMessageHub:
 
     def test_get_runtime(self):
         message_hub = MessageHub.get_instance('mmengine')
-        tgrresult = message_hub.get_info('unknown')
-        assert tgrresult is None
+        assert message_hub.get_info('unknown') is None
         recorded_dict = dict(a=1, b=2)
         message_hub.update_info('test_value', recorded_dict)
         assert message_hub.get_info('test_value') == recorded_dict
@@ -186,10 +185,8 @@ class TestMessageHub:
         obj = pickle.dumps(message_hub)
         instance = pickle.loads(obj)
 
-        featresult = instance.get_info('feat')
-        assert featresult is None
-        lrresult = instance.get_info('lr')
-        assert lrresult is None
+        assert instance.get_info('feat') is None
+        assert instance.get_info('lr') is None
 
         instance.get_info('iter')
         instance.get_scalar('loss')
