ad18f3b7cf72dee0a1f46c9963a98784113403b4
sunyinlei <ginray0215@gmail.com>
2023-04-25 17:35:33 +0800

    [Enhance] Enable full precision training on Ascend NPU.

--- mmengine/device/__init__.py
+++ mmengine/device/__init__.py
@@ -1,8 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .utils import (get_device, get_max_cuda_memory, is_cuda_available,
-                    is_mlu_available, is_mps_available, is_npu_available)
+                    is_mlu_available, is_mps_available, is_npu_available,
+                    is_npu_support_full_precision)
 
 __all__ = [
     'get_max_cuda_memory', 'get_device', 'is_cuda_available',
-    'is_mlu_available', 'is_mps_available', 'is_npu_available'
+    'is_mlu_available', 'is_mps_available', 'is_npu_available',
+    'is_npu_support_full_precision'
 ]
