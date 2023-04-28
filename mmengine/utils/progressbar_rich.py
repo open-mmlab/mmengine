da3fe1533071f0bf20c91aa76323c311cc6e50a0
yang_guo123 <1179039628@qq.com>
2023-04-28 10:07:14 +0800

    [Enhancement] change progressbar_rich.py

--- mmengine/utils/progressbar_rich.py
+++ mmengine/utils/progressbar_rich.py
@@ -1,233 +1,143 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from collections.abc import Iterable
-from functools import partial
 from multiprocessing import Pool
-from typing import Callable, Tuple
+from typing import Callable, Iterable, Sized
 
-import rich
-from rich.progress import Progress
-
-from ..utils import is_seq_of
-from .timer import Timer
+from rich.progress import (BarColumn, MofNCompleteColumn, Progress, Task,
+                           TaskProgressColumn, TextColumn, TimeRemainingColumn)
+from rich.text import Text
 
 
-class RichProgressBar:
-    """Use rich to enhance progress bar.
+class _Worker:
+    """Function wrapper for ``track_progress_rich``"""
 
-    This class uses the rich library to enhance the progressbar,
-    Do not update two RichProgressbars at the same time.
+    def __init__(self, func) -> None:
+        self.func = func
 
-    Examples:
-        >>> import mmengine
-        >>> import time
-        >>> bar = mmengine.RichProgressBar()
-        >>> bar.add_task(10)
-        >>> for i in range(10):
-        >>>     bar.update()
-        >>>     time.sleep(1)
+    def __call__(self, inputs):
+        inputs, idx = inputs
+        if not isinstance(inputs, (list, tuple, dict)):
+            inputs = (inputs, )
+        if isinstance(inputs, (tuple, list)):
+            return self.func(*inputs), idx
+        if isinstance(inputs, dict):
+            return self.func(**inputs), idx
+
+
+class _SkipFirstTimeRemainingColumn(TimeRemainingColumn):
+    """Skip calculating remaining time for the first few times.
+
+    Args:
+        skip_times (int): The number of times to skip. Defaults to 0.
     """
 
-    def __init__(self, *args, **kwargs):
-        self.bar = Progress(*args, **kwargs)
-        self.tasks = []
-        self.descriptions = []
-        self.colors = []
-        self.timer = None
-        self.completed = 0
-        self.infinite = False
+    def __init__(self, *args, skip_times=0, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.skip_times = skip_times
 
-    @staticmethod
-    def write(msg: str, color: str = 'blue') -> None:
-        """Write the massage.
-
-        Args:
-            msg (str): Output massage.
-            color (str, optional): Text color.
-        """
-        rich.print(f'[{color}]{msg}')
-
-    def add_task(self,
-                 total: int = None,
-                 color: str = 'blue',
-                 description: str = 'Process...') -> int:
-        """Adding tasks to RichProgressbar.
-
-        Args:
-            total (int, optional): Number of total steps, When the input is
-                None, it indicates a task with unknown length. Defaults to
-                None.
-            color (str): Color of progress bar. Defaults to "blue".
-            description (str): Description of progress bar.
-                Defaults to "Process...".
-        Returns:
-            int: Added task`s id.
-        """
-        if total is not None:
-            assert not self.infinite, (
-                'The prior task is an infinite task (total is None), '
-                'RichProgressBar can only accept one infinite task')
-
-            if total <= 0:
-                raise ValueError(
-                    'Total only exists if it is greater than zero or None.')
-            self.tasks.append(
-                self.bar.add_task(
-                    f'[{color}]{description}_0/{total}', total=total))
-            self.colors.append(color)
-            self.descriptions.append(description)
-            task_id = self.bar.tasks[-1].id
-        else:
-            assert not self.tasks, (
-                'Since the total argument is None, this task is considered '
-                'infinite. RichProgressBar should not have any other tasks '
-                'added to it when an infinite task is present.')
-            self.write('completed: 0, elapsed: 0s', color)
-            self.timer = Timer()
-            self.infinite = True
-            self.completed = 0
-            self.tasks.append(0)
-            self.colors.append(color)
-            self.descriptions.append(description)
-            task_id = 0
-        return task_id
-
-    def update(self, task_id: int = 0, advance: int = 1) -> None:
-        """update progressbar.
-
-        Args:
-            task_id (int): Task ID that needs to be updated. Defaults to 0.
-            advance (int): Update step size. Defaults to 1.
-        """
-        if advance <= 0:
-            raise ValueError('advance should be greater than zero.')
-
-        if self.infinite:
-            if task_id != 0:
-                raise ValueError('In Infinite mode, task_ ID must be 0.')
-
-            self.completed += advance
-            elapsed = self.timer.since_start()
-            if elapsed > 0:
-                fps = self.completed / elapsed
-            else:
-                fps = float('inf')
-            self.write(
-                f'completed: {self.completed}, '
-                f'elapsed: {int(elapsed + 0.5)}s, '
-                f'{fps:.1f} tasks/s', self.colors[task_id])
-        else:
-            if task_id < 0 or task_id > len(self.tasks):
-                raise ValueError('The task_id must be within a valid range')
-
-            # Activate self.bar before all tasks have been updated.
-            if all(task.completed == 0 for task in self.bar.tasks):
-                self.bar.start()
-
-            completed = self.bar.tasks[task_id].completed + advance
-            total = self.bar.tasks[task_id].total
-            self.bar.update(
-                task_id,
-                advance=advance,
-                description=f'[{self.colors[task_id]}]'
-                f'{self.descriptions[task_id]}'
-                f'_{completed}/{total}')
-
-            # After all tasks are completed, deactivate self.bar.
-            if all(task.finished for task in self.bar.tasks):
-                self.bar.stop()
+    def render(self, task: Task) -> Text:
+        """Show time remaining."""
+        if task.completed <= self.skip_times:
+            return Text('-:--:--', style='progress.remaining')
+        return super().render(task)
 
 
-def worker(params, function):
-    """Used for multithreaded functions."""
-    result = function(*params)
-    return result
+def _tasks_with_index(tasks):
+    """Add index to tasks."""
+    for idx, task in enumerate(tasks):
+        yield task, idx
 
 
 def track_progress_rich(func: Callable,
-                        tasks: Tuple[list],
+                        tasks: Iterable = tuple(),
+                        task_num: int = None,
                         nproc: int = 1,
-                        description: str = 'Process...',
-                        color: str = 'blue',
                         chunksize: int = 1,
-                        skip_first: bool = False,
-                        keep_order: bool = True) -> list:
+                        description: str = 'Process...',
+                        color: str = 'blue') -> list:
     """Track the progress of parallel task execution with a progress bar.
-
     The built-in :mod:`multiprocessing` module is used for process pools and
     tasks are done with :func:`Pool.map` or :func:`Pool.imap_unordered`.
-
     Args:
         func (callable): The function to be applied to each task.
-        tasks (Tuple[list]): A tuple of tasks.
+        tasks (Iterable or Sized): A tuple of tasks. There are several cases
+            for different format tasks:
+            - When ``func`` accepts no arguments: tasks should be an empty
+              tuple, and ``task_num`` must be specified.
+            - When ``func`` accepts only one argument: tasks should be a tuple
+              containing the argument.
+            - When ``func`` accepts multiple arguments: tasks should be a
+              tuple, with each element representing a set of arguments.
+              If an element is a ``dict``, it will be parsed as a set of
+              keyword-only arguments.
+            Defaults to an empty tuple.
+        task_num (int, optional): If ``tasks`` is an iterator which does not
+            have length, the number of tasks can be provided by ``task_num``.
+            Defaults to None.
         nproc (int): Process (worker) number, if nuproc is 1,
             use single process. Defaults to 1.
+        chunksize (int): Refer to :class:`multiprocessing.Pool` for details.
+            Defaults to 1.
         description (str): The description of progress bar.
             Defaults to "Process".
         color (str): The color of progress bar. Defaults to "blue".
-        chunksize (int): Refer to :class:`multiprocessing.Pool` for details.
-            Defaults to 1.
-        skip_first (bool): Whether to skip the first sample for each worker
-            when estimating fps, since the initialization step may takes
-            longer. Defaults to False.
-        keep_order (bool): If True, :func:`Pool.imap` is used, otherwise
-            :func:`Pool.imap_unordered` is used. Defaults to True.
-
     Returns:
         list: The task results.
     """
-    if not is_seq_of(tasks, Iterable):
-        raise ValueError('The content of tasks must be iterable object')
+    if not callable(func):
+        raise TypeError('func must be a callable object')
+    if not isinstance(tasks, Iterable):
+        raise TypeError(
+            f'tasks must be an iterable object, but got {type(tasks)}')
+    if not isinstance(tasks, Sized):
+        task_num = None or task_num
+    elif len(tasks) == 0 and task_num is None:
+        raise ValueError('If tasks is an empty iterable, '
+                         'task_num must be set')
+    elif len(tasks) == 0:
+        tasks = tuple(tuple() for _ in range(10))
+    else:
+        if task_num is not None and task_num != len(tasks):
+            raise ValueError('task_num does not match the length of tasks')
+        task_num = len(tasks)
 
     if nproc <= 0:
         raise ValueError('nproc must be a positive number')
 
-    if not hasattr(tasks[0], '__len__'):
-        task_num = None
-    else:
-        if len({len(arg) for arg in tasks}) != 1:
-            raise ValueError('args must have the same length, '
-                             'please check each argument in your '
-                             'tasks has the same length')
+    skip_times = nproc * chunksize if nproc > 1 else 0
+    prog_bar = Progress(
+        TextColumn('{task.description}'),
+        BarColumn(),
+        _SkipFirstTimeRemainingColumn(skip_times=skip_times),
+        MofNCompleteColumn(),
+        TaskProgressColumn(show_speed=True),
+    )
 
-        task_num = len(tasks[0])
-
-    prog_bar = RichProgressBar()
+    worker = _Worker(func)
+    task_id = prog_bar.add_task(
+        total=task_num, color=color, description=description)
+    tasks = _tasks_with_index(tasks)
 
     # Use single process when nproc is 1, else use multiprocess.
-    if nproc == 1:
-        prog_bar.add_task(task_num, color=color, description=description)
-        results = []
-        for task in zip(*tasks):
-            results.append(func(*task))
-            prog_bar.update()
-        return results
-    else:
-        param = list(zip(*tasks))
-        work = partial(worker, function=func)
-
-        pool = Pool(nproc)
-        if task_num is not None:
-            task_num -= nproc * chunksize * int(skip_first)
-
-        prog_bar.add_task(task_num, description=description, color=color)
-        results = []
-        if keep_order:
-            gen = pool.imap(work, param, chunksize)
+    with prog_bar:
+        if nproc == 1:
+            results = []
+            for task in tasks:
+                results.append(worker(task)[0])
+                prog_bar.update(task_id, advance=1, refresh=True)
         else:
-            gen = pool.imap_unordered(work, param, chunksize)
-
-        try:
-            for result in gen:
-                results.append(result)
-                if skip_first:
-                    if len(results) <= nproc * chunksize:
-                        continue
-                prog_bar.update()
-        except Exception as e:
-            prog_bar.bar.stop()
-            raise e
-
-        pool.close()
-        pool.join()
-
-        return results
+            with Pool(nproc) as pool:
+                results = []
+                unordered_results = []
+                gen = pool.imap_unordered(worker, tasks, chunksize)
+                try:
+                    for result in gen:
+                        result, idx = result
+                        unordered_results.append((result, idx))
+                        results.append(None)
+                        prog_bar.update(task_id, advance=1, refresh=True)
+                except Exception as e:
+                    prog_bar.stop()
+                    raise e
+            for result, idx in unordered_results:
+                results[idx] = result
+    return results
--- tests/test_utils/test_progressbar_rich.py
+++ tests/test_utils/test_progressbar_rich.py
@@ -1,100 +1,62 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import time
-
 import pytest
 
-import mmengine
+from mmengine.utils import track_progress_rich
 
 
-class TestRichProgressBar:
-
-    def test_init(self):
-        prog_bar = mmengine.RichProgressBar()
-        assert len(prog_bar.tasks) == 0
-        assert len(prog_bar.colors) == 0
-        assert len(prog_bar.descriptions) == 0
-        assert prog_bar.bar.live.is_started is False
-        assert prog_bar.infinite is False
-        assert prog_bar.timer is None
-        assert prog_bar.completed == 0
-
-    def test_add(self):
-        # single task
-        prog_bar = mmengine.RichProgressBar()
-        prog_bar.add_task(100)
-        assert len(prog_bar.bar.tasks) == 1
-        assert prog_bar.bar.tasks[0].total == 100
-        assert prog_bar.infinite is False
-        # multi tasks
-        prog_bar = mmengine.RichProgressBar()
-        for i in range(5):
-            prog_bar.add_task(10)
-        assert len(prog_bar.bar.tasks) == 5
-        # without total task num
-        prog_bar = mmengine.RichProgressBar()
-        prog_bar.add_task(None)
-        assert prog_bar.infinite is True
-        # test assert
-        prog_bar = mmengine.RichProgressBar()
-        prog_bar.add_task(None)
-        with pytest.raises(AssertionError):
-            prog_bar.add_task(10)
-        with pytest.raises(AssertionError):
-            prog_bar.add_task(None)
-        prog_bar = mmengine.RichProgressBar()
-        prog_bar.add_task(10)
-        with pytest.raises(AssertionError):
-            prog_bar.add_task(None)
-
-    def test_update(self):
-        # single task
-        prog_bar = mmengine.RichProgressBar()
-        prog_bar.add_task(10)
-        for i in range(10):
-            prog_bar.update()
-        assert prog_bar.bar.tasks[0].finished is True
-        # without total task num
-        prog_bar = mmengine.RichProgressBar()
-        prog_bar.add_task(None)
-        for i in range(10):
-            prog_bar.update()
-        assert prog_bar.completed == 10
-        # multi task
-        prog_bar = mmengine.RichProgressBar()
-        task_ids = []
-        for i in range(10):
-            task_ids.append(prog_bar.add_task(10))
-        for i in range(10):
-            for idx in task_ids:
-                prog_bar.update(idx)
-        assert prog_bar.bar.finished is True
-        for idx in task_ids:
-            assert prog_bar.bar.tasks[idx].finished is True
+def foo():
+    return 1
 
 
-def add(x, y):
-    time.sleep(1)
-    return x + y
+def foo1(x):
+    return x
 
 
-def test_track_progress():
-    ret = mmengine.track_progress_rich(add, ([1, 2, 3], [4, 5, 6]))
-    assert ret == [5, 7, 9]
+def foo2(x, y):
+    return x, y
 
 
-def test_track_parallel_progress():
-    results = mmengine.track_progress_rich(
-        add, ([1, 2, 3], [4, 5, 6]), nproc=3)
-    assert results == [5, 7, 9]
+def foo3(*, x, y):
+    return x, y
 
 
-def test_track_parallel_progress_skip_first():
-    results = mmengine.track_progress_rich(
-        add, ([1, 2, 3, 4], [2, 3, 4, 5]), nproc=2, skip_first=True)
-    assert results == [3, 5, 7, 9]
+def test_progressbar_rich_exception():
+    tasks = [1] * 10
+    # Valid func
+    with pytest.raises(TypeError):
+        track_progress_rich(1, tasks)
+    # invalid task
+    with pytest.raises(TypeError):
+        track_progress_rich(foo1, 1)
+    # mismatched task number
+    with pytest.raises(ValueError):
+        track_progress_rich(foo1, tasks, task_num=9)
+    # invalid proc
+    with pytest.raises(ValueError):
+        track_progress_rich(foo1, tasks, nproc=0)
+    # empty tasks and task_num is None
+    with pytest.raises(ValueError):
+        track_progress_rich(foo1, nproc=0)
 
 
-def test_track_parallel_progress_iterator():
-    results = mmengine.track_progress_rich(
-        add, (iter([1, 2, 3, 4]), iter([2, 3, 4, 5])), nproc=3)
-    assert results == [3, 5, 7, 9]
+@pytest.mark.parametrize('nproc', [1, 2])
+def test_progressbar_rich(nproc):
+    # empty tasks
+    results = track_progress_rich(foo, nproc=nproc, task_num=10)
+    assert results == [1] * 10
+    # Ordered results
+    # foo1
+    tasks_ = [i for i in range(10)]
+    for tasks in (tasks_, iter(tasks_)):
+        results = track_progress_rich(foo1, tasks, nproc=nproc)
+        assert results == tasks_
+    # foo2
+    tasks_ = [(i, i + 1) for i in range(10)]
+    for tasks in (tasks_, iter(tasks_)):
+        results = track_progress_rich(foo2, tasks, nproc=nproc)
+        assert results == tasks_
+    # foo3
+    tasks_ = [{'x': i, 'y': i} for i in range(10)]
+    for tasks in (tasks_, iter(tasks_)):
+        results = track_progress_rich(foo3, tasks, nproc=nproc)
+        assert results == [(i, i) for i in range(10)]
