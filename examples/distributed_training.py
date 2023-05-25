7426592415d75bb12aa3501b9761466355b02566
HAOCHENYE <21724054@zju.edu.cn>
2023-05-25 15:29:13 +0800

    clean the code

--- examples/distributed_training.py
+++ examples/distributed_training.py
@@ -93,9 +93,7 @@ def main():
         val_cfg=dict(),
         val_evaluator=dict(type=Accuracy),
         launcher=args.launcher,
-        visualizer=dict(
-            type='Visualizer', vis_backends=[dict(type='ClearMLVisBackend')]),
-        cfg=dict(a=1, b=2))
+    )
     runner.train()
 
 
