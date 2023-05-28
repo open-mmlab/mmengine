0ff89f7c36d4b99a1537f3b9d3848639cd974fce
Adrian Joshua Strutt <60191560+adrianjoshua-strutt@users.noreply.github.com>
2023-05-28 15:30:36 +0200

    [Enhancement] Handle the case for Multi-Instance GPUs when using cuda_visible_devices (#1164)

--- mmengine/logging/logger.py
+++ mmengine/logging/logger.py
@@ -376,7 +376,12 @@ def _get_device_id():
             cuda_visible_devices = list(range(num_device))
         else:
             cuda_visible_devices = cuda_visible_devices.split(',')
-        return int(cuda_visible_devices[local_rank])
+        if cuda_visible_devices[local_rank].isdigit():
+            return int(cuda_visible_devices[local_rank])
+        else:
+            # handle case for Multi-Instance GPUs
+            # see #1148 for details
+            return cuda_visible_devices[local_rank]
 
 
 def _get_host_info() -> str:
