0e8a519183c031777b1b65495f3f52621235c64e
HAOCHENYE <21724054@zju.edu.cn>
2023-06-20 22:11:02 +0800

    Add docstring for read_base
    Add docstring for read_base

--- mmengine/config/config.py
+++ mmengine/config/config.py
@@ -1731,5 +1731,15 @@ class DictAction(Action):
 
 @contextmanager
 def read_base():
-    """Context manager to mark the base config."""
+    """Context manager to mark the base config.
+
+    The pure Python-style configuration file allows you to use the import
+    syntax. However, it is important to note that you need to import the base
+    configuration file within the context of ``read_base``, and import other
+    dependencies outside of it.
+
+    You can see more usage of Python-style configuration in the `tutorial`_
+
+    .. _tutorial: https://mmengine.readthedocs.io/en/latest/advanced_tutorials/config.html#a-pure-python-style-configuration-file-beta
+    """  # noqa: E501
     yield
