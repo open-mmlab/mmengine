1b7c954ebef5640a698ef9270021df4760712817
HAOCHENYE <21724054@zju.edu.cn>
2023-06-28 15:42:15 +0800

    tmp
    1. refine metric 2. Visualize the seg result

--- examples/segmentation/README.md
+++ examples/segmentation/README.md
@@ -20,50 +20,11 @@ pip install -U opendatalab
 # Login
 odl login
 # Download this dataset
-odl get    CamVid
-# preprocess data
-python prepare_data.py
-```
-
-Or, after register your account, you can use the following code to download the dataset.
-
-```python
-import os
-import shutil
-import subprocess
-import tarfile
-
-try:
-    # Install and upgrade opendatalab
-    subprocess.check_call(['pip', 'install', 'opendatalab'],
-                          stdout=subprocess.DEVNULL,
-                          stderr=subprocess.DEVNULL)
-    print('Installed opendatalab.\n')
-    subprocess.check_call(['pip', 'install', '-U', 'opendatalab'],
-                          stdout=subprocess.DEVNULL,
-                          stderr=subprocess.DEVNULL)
-    print('Upgraded opendatalab.\n')
-except subprocess.CalledProcessError as e:
-    print(f'Error: {e}')
-
-# Check if the file exists
-file_path = 'CamVid/raw/CamVid.tar.gz.00'
-if not os.path.exists(file_path):
-    print('Downloading dataset from https://opendatalab.com/CamVid.\n')
-    print('You need register an account before downloading the dataset.\n')
-    print('Please input your username and password to login to OpenDataLab.')
-    # Login to OpenDataLab (Please manually enter your credentials when asked)
-    subprocess.check_call(['odl', 'login'])
-    # Download the dataset
-    subprocess.check_call(['odl', 'get', 'CamVid'])
-
-# Unzip dataset
-with tarfile.open(file_path, 'r:gz') as tar:
-    tar.extractall('data')
-    print('Extracted dataset to /data.\n')
-
-# Delete the CamVid folder
-shutil.rmtree('CamVid')
+mkdir data
+odl get CamVid -d data
+# Preprocess data in Linux. You should extract the files to data manually in
+# Windows
+tar -xzvf data/CamVid/raw/CamVid.tar.gz.00 -C ./data
 ```
 
 ## Building Camvid Dataset
--- examples/segmentation/segmentation_training.py
+++ examples/segmentation/segmentation_training.py
@@ -1,24 +1,28 @@
 import argparse
 import csv
 import os
+import os.path as osp
+import shutil
 
+import cv2
 import numpy as np
 import torch
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 from PIL import Image
-from torch.optim import SGD
+from torch.optim import AdamW
 from torchvision.datasets import VisionDataset
 from torchvision.models.segmentation import deeplabv3_resnet50
 from torchvision.models.segmentation.deeplabv3 import \
     DeepLabV3_ResNet50_Weights
 
 from mmengine.evaluator import BaseMetric
+from mmengine.hooks import Hook
 from mmengine.model import BaseModel
 from mmengine.runner import Runner
 
 
-def create_color_to_class_mapping(csv_filepath):
+def create_palette(csv_filepath):
     color_to_class = {}
     with open(csv_filepath, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -26,60 +30,6 @@ def create_color_to_class_mapping(csv_filepath):
             r, g, b = int(row['r']), int(row['g']), int(row['b'])
             color_to_class[(r, g, b)] = idx
     return color_to_class
-
-
-class MMDeeplabV3(BaseModel):
-
-    def __init__(self, num_classes, device):
-        super().__init__()
-        self.deeplab = deeplabv3_resnet50(
-            weights=DeepLabV3_ResNet50_Weights.COCO_WITH_VOC_LABELS_V1,
-            num_classes=21)
-        self.deeplab.classifier[4] = torch.nn.Conv2d(
-            256, num_classes, kernel_size=(1, 1), stride=(1, 1))
-        self.device = device
-        self.deeplab = self.deeplab.to(self.device)  # Move model to device
-
-    def forward(self, imgs, labels, mode):
-        imgs = imgs.to(self.device)  # Move images to device
-        labels = labels.to(self.device)  # Move labels to device
-        x = self.deeplab(imgs)['out']
-        labels = labels.squeeze(
-            1)  # Make sure to remove the second dimension of labels.
-        if mode == 'loss':
-            return {'loss': F.cross_entropy(x, labels, ignore_index=255)}
-        elif mode == 'predict':
-            return x, labels
-
-
-class IoU(BaseMetric):
-
-    def process(self, data_batch, data_samples):
-        score, gt = data_samples
-        pred = torch.argmax(score, dim=1)
-        gt = gt.where(gt != 255, torch.tensor(-1, device=gt.device))
-        intersection = (pred * gt).sum(dim=(1, 2)).float()
-        union = (pred + gt).sum(dim=(1, 2)).float() - intersection
-        iou = (intersection / union).cpu()
-        self.results.append({'batch_size': len(gt), 'iou': iou})
-
-    def compute_metrics(self, results):
-        total_iou = sum(item['iou'].sum() for item in results)
-        total_size = sum(item['batch_size'] for item in results)
-        return dict(iou=100 * total_iou / total_size)
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description='Distributed Training')
-    parser.add_argument(
-        '--launcher',
-        choices=['none', 'pytorch', 'slurm', 'mpi'],
-        default='none',
-        help='job launcher')
-    parser.add_argument('--local_rank', type=int, default=0)
-
-    args = parser.parse_args()
-    return args
 
 
 class CamVid(VisionDataset):
@@ -98,7 +48,7 @@ class CamVid(VisionDataset):
             sorted(os.listdir(os.path.join(self.root, img_folder))))
         self.masks = list(
             sorted(os.listdir(os.path.join(self.root, mask_folder))))
-        self.color_to_class = create_color_to_class_mapping(
+        self.color_to_class = create_palette(
             os.path.join(self.root, 'class_dict.csv'))
 
     def __getitem__(self, index):
@@ -115,25 +65,116 @@ class CamVid(VisionDataset):
         # Convert the RGB values to class indices
         mask = np.array(mask)
         mask = mask[:, :, 0] * 65536 + mask[:, :, 1] * 256 + mask[:, :, 2]
-        mask_class = np.zeros_like(mask, dtype=np.int64)
+        labels = np.zeros_like(mask, dtype=np.int64)
         for color, class_index in self.color_to_class.items():
             rgb = color[0] * 65536 + color[1] * 256 + color[2]
-            mask_class[mask == rgb] = class_index
+            labels[mask == rgb] = class_index
 
         if self.target_transform is not None:
-            mask_class = self.target_transform(mask_class)
-
-        return img, mask_class
+            labels = self.target_transform(labels)
+        data_samples = dict(
+            labels=labels, img_path=img_path, mask_path=mask_path)
+        return img, data_samples
 
     def __len__(self):
         return len(self.images)
+
+
+class MMDeeplabV3(BaseModel):
+
+    def __init__(self, num_classes):
+        super().__init__()
+        self.deeplab = deeplabv3_resnet50(
+            weights=DeepLabV3_ResNet50_Weights.COCO_WITH_VOC_LABELS_V1,
+            num_classes=21)
+        self.deeplab.classifier[4] = torch.nn.Conv2d(
+            256, num_classes, kernel_size=(1, 1), stride=(1, 1))
+
+    def forward(self, imgs, data_samples=None, mode='tensor'):
+        x = self.deeplab(imgs)['out']
+        if mode == 'loss':
+            return {'loss': F.cross_entropy(x, data_samples['labels'])}
+        elif mode == 'predict':
+            return x, data_samples
+
+
+class IoU(BaseMetric):
+
+    def process(self, data_batch, data_samples):
+        preds, labels = data_samples[0], data_samples[1]['labels']
+        preds = torch.argmax(preds, dim=1)
+        intersect = (labels == preds).sum()
+        union = (torch.logical_or(preds, labels)).sum()
+        iou = (intersect / union).cpu()
+        self.results.append(
+            dict(batch_size=len(labels), iou=iou * len(labels)))
+
+    def compute_metrics(self, results):
+        total_iou = sum(result['iou'] for result in self.results)
+        num_samples = sum(result['batch_size'] for result in self.results)
+        return dict(iou=total_iou / num_samples)
+
+
+class SegVisHook(Hook):
+
+    def __init__(self, data_root, vis_num=1) -> None:
+        super().__init__()
+        self.vis_num = vis_num
+        self.palette = create_palette(osp.join(data_root, 'class_dict.csv'))
+
+    def after_val_iter(self,
+                       runner,
+                       batch_idx: int,
+                       data_batch=None,
+                       outputs=None) -> None:
+        if batch_idx > self.vis_num:
+            return
+        preds, data_samples = outputs
+        img_paths = data_samples['img_path']
+        mask_paths = data_samples['mask_path']
+        _, C, H, W = preds.shape
+        preds = torch.argmax(preds, dim=1)
+        for idx, (pred, img_path,
+                  mask_path) in enumerate(zip(preds, img_paths, mask_paths)):
+            pred_mask = np.zeros((H, W, 3), dtype=np.uint8)
+            runner.visualizer.set_image(pred_mask)
+            for color, class_id in self.palette.items():
+                runner.visualizer.draw_binary_masks(
+                    pred == class_id,
+                    colors=[color],
+                    alphas=1.0,
+                )
+            # Convert RGB to BGR
+            pred_mask = runner.visualizer.get_image()[..., ::-1]
+            saved_dir = osp.join(runner.log_dir, 'vis_data', str(idx))
+            os.makedirs(saved_dir, exist_ok=True)
+
+            shutil.copyfile(img_path,
+                            osp.join(saved_dir, osp.basename(img_path)))
+            shutil.copyfile(mask_path,
+                            osp.join(saved_dir, osp.basename(mask_path)))
+            cv2.imwrite(
+                osp.join(saved_dir, f'pred_{osp.basename(img_path)}'),
+                pred_mask)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Distributed Training')
+    parser.add_argument(
+        '--launcher',
+        choices=['none', 'pytorch', 'slurm', 'mpi'],
+        default='none',
+        help='job launcher')
+    parser.add_argument('--local_rank', type=int, default=0)
+
+    args = parser.parse_args()
+    return args
 
 
 def main():
     args = parse_args()
     num_classes = 32  # Modify to actual number of categories.
     norm_cfg = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     transform = transforms.Compose(
         [transforms.ToTensor(),
@@ -157,7 +198,7 @@ def main():
         target_transform=target_transform)
 
     train_dataloader = dict(
-        batch_size=3,
+        batch_size=12,
         dataset=train_set,
         sampler=dict(type='DefaultSampler', shuffle=True),
         collate_fn=dict(type='default_collate'))
@@ -168,11 +209,11 @@ def main():
         collate_fn=dict(type='default_collate'))
 
     runner = Runner(
-        model=MMDeeplabV3(num_classes, device),
+        model=MMDeeplabV3(num_classes),
         work_dir='./work_dir',
         train_dataloader=train_dataloader,
-        optim_wrapper=dict(optimizer=dict(type=SGD, lr=0.01, momentum=0.9)),
-        train_cfg=dict(by_epoch=True, max_epochs=2, val_interval=1),
+        optim_wrapper=dict(optimizer=dict(type=AdamW, lr=1e-3)),
+        train_cfg=dict(by_epoch=True, max_epochs=10, val_interval=5),
         val_dataloader=val_dataloader,
         val_cfg=dict(),
         val_evaluator=dict(type=IoU),
@@ -180,10 +221,8 @@ def main():
         cfg=dict(
             model_wrapper='MMDistributedDataParallel',
             find_unused_parameters=True),
+        custom_hooks=[SegVisHook('data/CamVid')],
     )
-    if torch.cuda.is_available(
-    ):  # Added this line to clear GPU cache before each training iteration
-        torch.cuda.empty_cache()
     runner.train()
 
 
