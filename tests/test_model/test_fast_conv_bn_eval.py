09badf1711f442d0cb7ddb649d0461826672ac84
youkaichao <youkaichao@126.com>
2023-07-13 17:31:12 +0800

    update test code

--- tests/test_model/test_fast_conv_bn_eval.py
+++ tests/test_model/test_fast_conv_bn_eval.py
@@ -1,29 +1,27 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import unittest
 from unittest import TestCase
 
 import torch
 from torch import nn
 
+import mmengine.model.fast_conv_bn_eval as mod
+from mmengine.model.fast_conv_bn_eval import \
+    turn_on_fast_conv_bn_eval_for_single_model
 from mmengine.testing import assert_allclose
 
-should_test = True
-
-try:
-    from mmengine.model.fast_conv_bn_eval import \
-        turn_on_fast_conv_bn_eval_for_single_model
-except RuntimeError:
-    should_test = False
+optimize_conv_module = mod.optimize_conv_module
 
 
 class BackboneModel(nn.Module):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        from mmcv.cnn import ConvModule
-        conv0 = nn.Conv2d(6, 6, 6)
-        bn0 = nn.BatchNorm2d(6)
-        self.mod1 = ConvModule.create_from_conv_bn(conv0, bn0)
+        global optimize_conv_module
+        if optimize_conv_module:
+            from mmcv.cnn import ConvModule
+            conv0 = nn.Conv2d(6, 6, 6)
+            bn0 = nn.BatchNorm2d(6)
+            self.mod1 = ConvModule.create_from_conv_bn(conv0, bn0)
         self.conv1 = nn.Conv2d(6, 6, 6)
         self.bn1 = nn.BatchNorm2d(6)
         self.conv2 = nn.Conv2d(6, 6, 6)
@@ -32,8 +30,10 @@ class BackboneModel(nn.Module):
         self.bn3 = nn.BatchNorm2d(6)
 
     def forward(self, x):
-        # this ConvModule can use fast_conv_bn_eval feature
-        x = self.mod1(x)
+        global optimize_conv_module
+        if optimize_conv_module:
+            # this ConvModule can use fast_conv_bn_eval feature
+            x = self.mod1(x)
         # this conv-bn pair can use fast_conv_bn_eval feature
         x = self.bn1(self.conv1(x))
         # this conv-bn pair cannot use fast_conv_bn_eval feature
@@ -45,7 +45,6 @@ class BackboneModel(nn.Module):
         return x
 
 
-@unittest.skipIf(not should_test, reason='package requirement not satisfied')
 class TestFastConvBNEval(TestCase):
     """Test the turn_on_fast_conv_bn_eval function."""
 
