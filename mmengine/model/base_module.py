ea8e05c92199b1c9d381f0fa47f9c2a60a642a44
HAOCHENYE <21724054@zju.edu.cn>
2023-07-20 14:21:25 +0800

    fix

--- mmengine/model/base_module.py
+++ mmengine/model/base_module.py
@@ -131,7 +131,7 @@ class BaseModule(nn.Module, metaclass=ABCMeta):
             for m in self.children():
                 if is_model_wrapper(m) and not hasattr(m, 'init_weights'):
                     m = m.module
-                if hasattr(m, 'init_weights') and getattr(
+                if hasattr(m, 'init_weights') and not getattr(
                         m, '_is_init', False):
                     m.init_weights()
                     # users may overload the `init_weights`
