7eb79a37e5d332eeee601f54217eb7e433e1fa4e
黄启元 <josh66@163.com>
2023-07-21 14:15:07 +0800

    Update utils.py

--- mmengine/dist/utils.py
+++ mmengine/dist/utils.py
@@ -101,10 +101,11 @@ def _init_dist_pytorch(backend, init_backend='torch', **kwargs) -> None:
     rank = int(os.environ['RANK'])
     if is_mlu_available():
         import torch_mlu  # noqa: F401
-        torch.mlu.set_device(rank)
+        local_rank = int(os.environ['LOCAL_RANK'])
+        torch.mlu.set_device(local_rank)
         torch_dist.init_process_group(
             backend='cncl',
-            rank=rank,
+            rank=local_rank,
             world_size=int(os.environ['WORLD_SIZE']),
             **kwargs)
     elif is_npu_available():
