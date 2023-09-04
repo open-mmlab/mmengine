c6fac8ab62ccee4d31cae7f370069b61c8d34db1
Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>
2023-09-04 22:55:41 +0800

    Update __init__.py

--- mmengine/runner/__init__.py
+++ mmengine/runner/__init__.py
@@ -7,7 +7,7 @@ from .checkpoint import (CheckpointLoader, find_latest_checkpoint,
                          get_mmcls_models, get_state_dict,
                          get_torchvision_models, load_checkpoint,
                          load_state_dict, save_checkpoint, weights_to_cpu)
-from .gradient_checkpoint import turn_on_gradient_checkpoint
+from .activation_checkpointing import turn_on_activation_checkpointing
 from .log_processor import LogProcessor
 from .loops import EpochBasedTrainLoop, IterBasedTrainLoop, TestLoop, ValLoop
 from .priority import Priority, get_priority
@@ -21,5 +21,5 @@ __all__ = [
     'save_checkpoint', 'EpochBasedTrainLoop', 'IterBasedTrainLoop', 'ValLoop',
     'TestLoop', 'Runner', 'get_priority', 'Priority', 'find_latest_checkpoint',
     'autocast', 'LogProcessor', 'set_random_seed', 'FlexibleRunner',
-    'turn_on_gradient_checkpoint'
+    'turn_on_activation_checkpointing'
 ]
