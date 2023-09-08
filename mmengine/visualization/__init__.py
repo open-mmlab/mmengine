53d94709fb394d32661abe5885bf7bcff1e3f7a5
RangeKing <rangekinghz@gmail.com>
2023-09-08 22:01:01 +0800

    Add AimVisBackend

--- mmengine/visualization/__init__.py
+++ mmengine/visualization/__init__.py
@@ -1,11 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .vis_backend import (BaseVisBackend, ClearMLVisBackend, DVCLiveVisBackend,
-                          LocalVisBackend, MLflowVisBackend, NeptuneVisBackend,
-                          TensorboardVisBackend, WandbVisBackend)
+from .vis_backend import (AimVisBackend, BaseVisBackend, ClearMLVisBackend,
+                          DVCLiveVisBackend, LocalVisBackend, MLflowVisBackend,
+                          NeptuneVisBackend, TensorboardVisBackend,
+                          WandbVisBackend)
 from .visualizer import Visualizer
 
 __all__ = [
     'Visualizer', 'BaseVisBackend', 'LocalVisBackend', 'WandbVisBackend',
     'TensorboardVisBackend', 'MLflowVisBackend', 'ClearMLVisBackend',
-    'NeptuneVisBackend', 'DVCLiveVisBackend'
+    'NeptuneVisBackend', 'DVCLiveVisBackend', 'AimVisBackend'
 ]
