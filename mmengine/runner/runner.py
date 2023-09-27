c50c56e6e776799403a3052ad04ce6efa538f8e8
Zaida Zhou <58739961+zhouzaida@users.noreply.github.com>
2023-09-27 10:25:28 +0800

    Update mmengine/runner/runner.py

--- mmengine/runner/runner.py
+++ mmengine/runner/runner.py
@@ -463,7 +463,7 @@ class Runner:
             load_from=cfg.get('load_from'),
             resume=cfg.get('resume', False),
             launcher=cfg.get('launcher', 'none'),
-            env_cfg=cfg.get('env_cfg', dict(dist_cfg=dict(backend='nccl'))),  # type: ignore
+            env_cfg=cfg.get('env_cfg', dict(dist_cfg=dict(backend='nccl'))),  # noqa: E501
             log_processor=cfg.get('log_processor'),
             log_level=cfg.get('log_level', 'INFO'),
             visualizer=cfg.get('visualizer'),
