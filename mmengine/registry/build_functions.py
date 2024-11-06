7595f500e6f76bff509bbac015156e7e7148baf0
tenacious <tenacioustommy6@gmail.com>
2024-11-06 15:07:41 +0800

    change import level

--- mmengine/registry/build_functions.py
+++ mmengine/registry/build_functions.py
@@ -238,10 +238,10 @@ def build_optimizer_from_cfg(
         cfg: Union[dict, ConfigDict, Config],
         registry: Registry,
         default_args: Optional[Union[dict, ConfigDict, Config]] = None) -> Any:
+    from ..logging import print_log
     if 'type' in cfg \
             and 'Adafactor' == cfg['type'] \
             and digit_version(torch.__version__) >= digit_version('2.5.0'):
-        from ..logging import print_log
         print_log(
             'the torch version of Adafactor is registered as TorchAdafactor')
     return build_from_cfg(cfg, registry, default_args)
