61493167787dec0a7fd80a34a989f792061e22bb
MGAM <312065559@qq.com>
2025-04-25 13:42:58 +0000

    1. remove unnecessary distributed warp.

--- mmengine/model/wrappers/distributed.py
+++ mmengine/model/wrappers/distributed.py
@@ -127,7 +127,6 @@ class MMDistributedDataParallel(DistributedDataParallel):
         return log_vars
 
     def val_step(self, data: Union[dict, tuple, list]) -> list:
-        return self.module.val_step(data)
         """Gets the prediction of module during validation process.
 
         Args:
@@ -139,7 +138,6 @@ class MMDistributedDataParallel(DistributedDataParallel):
         return self.module.val_step(data)
 
     def test_step(self, data: Union[dict, tuple, list]) -> list:
-        return self.module.test_step(data)
         """Gets the predictions of module during testing process.
 
         Args:
