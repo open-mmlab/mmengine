61f9f0353df7812e1a0bf31d7b8d345c967efeee
HAOCHENYE <21724054@zju.edu.cn>
2025-10-25 18:20:33 +0000

    [Fix] Fix config bug in python312

--- mmengine/config/utils.py
+++ mmengine/config/utils.py
@@ -175,7 +175,7 @@ def _is_builtin_module(module_name: str) -> bool:
     origin_path = getattr(spec, 'origin', None)
     if origin_path is None:
         return False
-    if origin_path == "frozen":
+    if origin_path == 'frozen':
         return True
     origin_path = osp.abspath(origin_path)
     if ('site-package' in origin_path or 'dist-package' in origin_path
