e8a92b825874bd627a7da589cfc09c6326d81a9d
HAOCHENYE <21724054@zju.edu.cn>
2025-10-25 21:46:52 +0000

    [Fix] Fix unittest of empty cache hook

--- tests/test_hooks/test_empty_cache_hook.py
+++ tests/test_hooks/test_empty_cache_hook.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from copy import deepcopy
 from unittest.mock import patch
 
 import pytest
@@ -13,7 +14,7 @@ class TestEmptyCacheHook(RunnerTestCase):
                         reason='cuda should be available')
     def test_with_runner(self):
         with patch('torch.cuda.empty_cache') as mock_empty_cache:
-            cfg = self.epoch_based_cfg
+            cfg = deepcopy(self.epoch_based_cfg)
             cfg.custom_hooks = [dict(type='EmptyCacheHook')]
             cfg.train_cfg.val_interval = 1e6  # disable validation during training  # noqa: E501
             runner = self.build_runner(cfg)
@@ -24,12 +25,14 @@ class TestEmptyCacheHook(RunnerTestCase):
 
             # Call `torch.cuda.empty_cache` after each epoch:
             #   runner.train: `max_epochs` times.
+            #   runner.val: last epoch will always trigger validation (BC caused by `e258c848`)  # noqa: E501
             #   runner.val: `1` time.
             #   runner.test: `1` time.
-            target_called_times = runner.max_epochs + 2
+            target_called_times = runner.max_epochs + 3
             self.assertEqual(mock_empty_cache.call_count, target_called_times)
-
+        #
         with patch('torch.cuda.empty_cache') as mock_empty_cache:
+            cfg = deepcopy(self.epoch_based_cfg)
             cfg.custom_hooks = [dict(type='EmptyCacheHook', before_epoch=True)]
             runner = self.build_runner(cfg)
 
@@ -39,13 +42,15 @@ class TestEmptyCacheHook(RunnerTestCase):
 
             # Call `torch.cuda.empty_cache` after/before each epoch:
             #   runner.train: `max_epochs*2` times.
-            #   runner.val: `1*2` times.
+            #   runner.val: (max_epochs + 1)*2 times, last epoch will always trigger validation (BC caused by `e258c848`)  # noqa: E501
             #   runner.test: `1*2` times.
 
-            target_called_times = runner.max_epochs * 2 + 4
+            target_called_times = runner.max_epochs * 2 + (runner.max_epochs +
+                                                           1) * 2 + 1 * 2
             self.assertEqual(mock_empty_cache.call_count, target_called_times)
 
         with patch('torch.cuda.empty_cache') as mock_empty_cache:
+            cfg = deepcopy(self.epoch_based_cfg)
             cfg.custom_hooks = [
                 dict(type='EmptyCacheHook', after_iter=True, before_epoch=True)
             ]
@@ -57,13 +62,13 @@ class TestEmptyCacheHook(RunnerTestCase):
 
             # Call `torch.cuda.empty_cache` after/before each epoch,
             # after each iteration:
-            #   runner.train: `max_epochs*2 + len(dataloader)*max_epochs` times.  # noqa: E501
-            #   runner.val: `1*2 + len(val_dataloader)` times.
-            #   runner.test: `1*2 + len(val_dataloader)` times.
+            #   runner.train: max_epochs * (2 + len(train_dataloader)) times.
+            #   runner.val: (max_epochs + 1(interval) + 1(last)) * (2 + len(val_dataloader)) times  # noqa: E501
+            #   runner.test: 1 * (2 + len(test_dataloader)) times
 
             target_called_times = \
-                runner.max_epochs * 2 + 4 + \
-                len(runner.train_dataloader) * runner.max_epochs + \
-                len(runner.val_dataloader) + \
-                len(runner.test_dataloader)
+                runner.max_epochs * (2 + len(runner.train_dataloader)) + \
+                (runner.max_epochs + 1) * (2 + len(runner.val_dataloader)) + \
+                1 * (2 + len(runner.test_dataloader))
+
             self.assertEqual(mock_empty_cache.call_count, target_called_times)
