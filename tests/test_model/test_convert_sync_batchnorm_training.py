bfb3e4707b14ac4827b11cec0076b50d53fec71a
Jiahao Zhang <marchematics@gmail.com>
2026-08-31 00:17:55 +0800

    Test SyncBatchNorm training state

---
+++ tests/test_model/test_convert_sync_batchnorm_training.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+from mmengine.model import convert_sync_batchnorm
+
+
+def test_convert_sync_batchnorm_keeps_training_state():
+    bn = nn.BatchNorm2d(4)
+    bn.eval()
+
+    sync_bn = convert_sync_batchnorm(bn)
+
+    assert isinstance(sync_bn, nn.SyncBatchNorm)
+    assert sync_bn.training is False
